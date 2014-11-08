efcc8967ed0927d6e2e452392bee4dae68523b1a
Andre Medeiros <andre.medeiros@futurice.com>
2014-11-08 14:45:36 +0200

    Added Hello world example

---
+++ rx-run/examples/hello/hello.js
@@ -0,0 +1,33 @@
+var HelloModel = Cycle.defineModel(['changeName$'], function (intent) {
+  return {
+    name$: intent.changeName$.startWith('')
+  };
+});
+
+var HelloView = Cycle.defineView(['name$'], function (model) {
+  return {
+    vtree$: model.name$
+      .map(function (name) {
+        return Cycle.h('div', {}, [
+          Cycle.h('label', 'Name:'),
+          Cycle.h('input', {
+            'attributes': {'type': 'text'},
+            'ev-input': 'inputText$'
+          }),
+          Cycle.h('hr'),
+          Cycle.h('h1', 'Hello ' + name)
+        ]);
+      }),
+    events: ['inputText$']
+  };
+});
+
+var HelloIntent = Cycle.defineIntent(['inputText$'], function (view) {
+  return {
+    changeName$: view.inputText$
+      .map(function (ev) { return ev.target.value; })
+  }
+});
+
+Cycle.renderEvery(HelloView.vtree$, '.js-container');
+Cycle.connect(HelloModel, HelloView, HelloIntent);
