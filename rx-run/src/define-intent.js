ab8e016711c390639a178878b9e9f4367d6621cb
Andre Medeiros <andre.medeiros@futurice.com>
2014-11-12 22:42:56 +0200

    Updated defineModel etc to allow no input (and interface)

--- rx-run/src/define-intent.js
+++ rx-run/src/define-intent.js
@@ -2,13 +2,13 @@
 var BackwardFunction = require('./backward-function');
 var errors = require('./errors');
 
-function defineIntent(viewInterface, definitionFn) {
-  var intent = new BackwardFunction(viewInterface, definitionFn);
+function defineIntent() {
+  var intent = BackwardFunction.apply({}, arguments);
   intent = errors.customInterfaceErrorMessageInInject(intent,
     'Intent expects View to have the required property '
   );
   intent.clone = function () {
-    return defineIntent(viewInterface, definitionFn);
+    return defineIntent.apply({}, arguments);
   };
   return intent;
 }
---
+++ rx-run/test/models.js
@@ -0,0 +1,38 @@
+'use strict';
+/* global describe, it */
+var assert = require('assert');
+var Rx = require('rx');
+var Cycle = require('../src/cycle');
+
+describe('defineModel', function () {
+  it('should return an object when given interface and definitionFn', function () {
+    var model = Cycle.defineModel([], function () { return {}; });
+    assert.equal(typeof model, 'object');
+  });
+
+  it('should yield simple output when injected simple input', function (done) {
+    var model = Cycle.defineModel(['foo$'], function (input) {
+      return {
+        bar$: input.foo$.map(function () { return 'bar'; })
+      };
+    });
+    model.bar$.subscribe(function (x) {
+      assert.strictEqual(x, 'bar');
+      done();
+    });
+    model.inject({foo$: Rx.Observable.just('foo')});
+  });
+
+  it('should yield simple output even when injected nothing', function (done) {
+    var model = Cycle.defineModel(function () {
+      return {
+        bar$: Rx.Observable.just(246)
+      };
+    });
+    model.bar$.subscribe(function (x) {
+      assert.strictEqual(x, 246);
+      done();
+    });
+    model.inject();
+  });
+});
