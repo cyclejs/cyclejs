d5fbcc0e00fb1ec175b7e9185ed544f7467d4778
CMG <cmg@summa-tech.com>
2015-03-02 19:53:07 -0500

    Starting update of many example to support components

--- rx-run/examples/many/many-view.js
+++ rx-run/examples/many/many-view.js
@@ -9,30 +9,11 @@ var ManyView = Cycle.createView(function (Model) {
   }
 
   function vrenderItem(itemData) {
-    return h('div.item', {
-        style: {
-          border: '1px solid #000',
-          background: 'none repeat scroll 0% 0% ' + itemData.color,
-          width: itemData.width + 'px',
-          height: '70px',
-          display: 'block',
-          padding: '20px',
-          margin: '10px 0px'
-        }}, [
-        h('input.color-field', {
-          type: 'text',
-          attributes: {'data-item-id': itemData.id, value: itemData.color}
-        }),
-        h('div.slider-container', [
-          h('input.width-slider', {
-            type: 'range', min:'200', max:'1000',
-            attributes: {'data-item-id': itemData.id, value: itemData.width}
-          })
-        ]),
-        h('div.width-content', String(itemData.width)),
-        h('button.remove-btn', {attributes: {'data-item-id': itemData.id}}, 'Remove')
-      ]
-    );
+    return h('item.item', {
+      itemid: itemData.id,
+      color:  itemData.color,
+      width:  itemData.width
+    });
   }
 
   return {
