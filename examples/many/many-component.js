a60058e3d8467f4f94f27fb514b9bfa36023b6f8
CMG <cmg@summa-tech.com>
2015-03-02 19:53:07 -0500

    Starting update of many example to support components

---
+++ examples/many/many-component.js
@@ -0,0 +1,61 @@
+var h = Cycle.h;
+
+Cycle.registerCustomElement('item', function (User, Properties) {
+  var Model = Cycle.createModel(function (Intent, Properties) {
+    return {
+      id$: Properties.get('itemid$').shareReplay(1),
+      color$: Properties.get('color$').startWith('#888'),
+      width$: Properties.get('width$').startWith(200)
+    };
+  });
+
+  var View = Cycle.createView(function (Model) {
+    return {
+      vtree$: Cycle.Rx.Observable.combineLatest(
+        Model.get('id$'),
+        Model.get('color$'),
+        Model.get('width$'),
+        function (id, color, width) {
+          return h('div.item', {
+              key: id,
+              style: {
+                border: '1px solid #000',
+                background: 'none repeat scroll 0% 0% ' + color,
+                width: width + 'px',
+                height: '70px',
+                display: 'block',
+                padding: '20px',
+                margin: '10px 0px'
+              }}, [
+              h('input.color-field', {
+                type: 'text',
+                attributes: {'data-item-id': id, value: color}
+              }),
+              h('div.slider-container', [
+                h('input.width-slider', {
+                  type: 'range', min:'200', max:'1000',
+                  attributes: {'data-item-id': id, value: width}
+                })
+              ]),
+              h('div.width-content', String(width)),
+              h('button.remove-btn', {attributes: {'data-item-id': id}}, 'Remove')
+            ]
+          );
+        }
+      )
+    };
+  });
+
+  var Intent = Cycle.createIntent(function (User) {
+    return {
+      destroy$: User.event$('.remove-btn', 'click')
+    };
+  });
+
+  User.inject(View).inject(Model).inject(Intent, Properties)[0].inject(User);
+
+  return {
+    destroy$: Intent.get('destroy$')
+                .withLatestFrom(Model.get('id$'), function (ev, id) { return id; })
+  };
+});
