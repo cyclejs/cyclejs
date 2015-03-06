f60bf484c284915559ac811c7ccce28fc08080c9
CMG <cmg@summa-tech.com>
2015-03-06 17:04:40 -0500

    Finished updating many example

--- examples/many/many-component.js
+++ examples/many/many-component.js
@@ -3,7 +3,7 @@ var h = Cycle.h;
 Cycle.registerCustomElement('item', function (User, Properties) {
   var Model = Cycle.createModel(function (Intent, Properties) {
     return {
-      id$: Properties.get('itemid$').shareReplay(1),
+      id$:    Properties.get('itemid$').shareReplay(1),
       color$: Properties.get('color$').startWith('#888'),
       width$: Properties.get('width$').startWith(200)
     };
@@ -18,21 +18,22 @@ Cycle.registerCustomElement('item', function (User, Properties) {
         function (id, color, width) {
           return h('div.item', {
               style: {
-                border: '1px solid #000',
+                border:     '1px solid #000',
                 background: 'none repeat scroll 0% 0% ' + color,
-                width: width + 'px',
-                height: '70px',
-                display: 'block',
-                padding: '20px',
-                margin: '10px 0px'
-              }}, [
+                width:      width + 'px',
+                height:     '70px',
+                display:    'block',
+                padding:    '20px',
+                margin:     '10px 0px'
+              }
+            }, [
               h('input.color-field', {
-                type: 'text',
+                type:       'text',
                 attributes: {'data-item-id': id, value: color}
               }),
               h('div.slider-container', [
                 h('input.width-slider', {
-                  type: 'range', min:'200', max:'1000',
+                  type:       'range', min: '200', max: '1000',
                   attributes: {'data-item-id': id, value: width}
                 })
               ]),
@@ -47,14 +48,32 @@ Cycle.registerCustomElement('item', function (User, Properties) {
 
   var Intent = Cycle.createIntent(function (User) {
     return {
-      destroy$: User.event$('.remove-btn', 'click')
+      destroy$: User.event$('.remove-btn', 'click'),
+      changeColor$: User.event$('.color-field', 'input'),
+      changeWidth$: User.event$('.width-slider', 'input')
     };
   });
 
   User.inject(View).inject(Model).inject(Intent, Properties)[0].inject(User);
 
   return {
-    destroy$: Intent.get('destroy$')
-                .withLatestFrom(Model.get('id$'), function (ev, id) { return id; })
+    destroy$:     Intent.get('destroy$')
+                    .withLatestFrom(Model.get('id$'), function (ev, id) { return id; }),
+
+    changeColor$: Intent.get('changeColor$').withLatestFrom(Model.get('id$'),
+      function (ev, id) {
+        return {
+          id:    id,
+          color: ev.currentTarget.value
+        };
+      }),
+
+    changeWidth$: Intent.get('changeWidth$').withLatestFrom(Model.get('id$'),
+      function (ev, id) {
+        return {
+          id:    id,
+          width: Number(ev.currentTarget.value)
+        };
+      })
   };
 });
