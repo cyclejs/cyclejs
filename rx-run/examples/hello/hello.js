1e0364de71b060c424d3a2d7247d8ce167198f17
Andre Medeiros <andre.medeiros@futurice.com>
2015-04-03 23:00:22 +0300

    Introduce createSteam() and render() for issue #99
    This will likely break the build, no tests pass, but this commit
    is still a good milestone for the implementation of the feature/rewrite.

--- rx-run/examples/hello/hello.js
+++ rx-run/examples/hello/hello.js
@@ -1,32 +1,29 @@
-var HelloModel = Cycle.createModel(function (Intent) {
-  return {name$: Intent.get('changeName$').startWith('')};
+var h = Cycle.h;
+var Rx = Cycle.Rx;
+
+var name$ = Cycle.createStream(function model(changeName$) {
+  return changeName$.startWith('');
 });
 
-var HelloView = Cycle.createView(function (Model) {
-  return {
-    vtree$: Model.get('name$')
-      .map(function (name) {
-        return Cycle.h('div', {}, [
-          Cycle.h('label', 'Name:'),
-          Cycle.h('input.myinput', {attributes: {type: 'text'}}),
-          Cycle.h('hr'),
-          Cycle.h('h1', 'Hello ' + name)
-        ]);
-      })
-  };
+var vtree$ = Cycle.createStream(function view(name$) {
+  return name$.map(function (name) {
+    return h('div', [
+      h('label', 'Name:'),
+      h('input.myinput', {attributes: {type: 'text'}}),
+      h('hr'),
+      h('h1', 'Hello ' + name)
+    ]);
+  });
 });
 
-var HelloUser = Cycle.createDOMUser('.js-container');
-
-var HelloIntent = Cycle.createIntent(function (User) {
-  return {
-    changeName$: User.event$('.myinput', 'input')
-      .map(function (ev) { return ev.target.value; })
-  };
+var interactions$ = Cycle.createStream(function user(vtree$) {
+  return Cycle.render(vtree$, '.js-container').getInteractions$();
 });
 
-HelloUser
-.inject(HelloView)
-.inject(HelloModel)
-.inject(HelloIntent)
-.inject(HelloUser);
+var changeName$ = Cycle.createStream(function intent(interactions$) {
+  return interactions$.choose('.myinput', 'input').map(function (ev) {
+    return ev.target.value;
+  });
+});
+
+interactions$.inject(vtree$).inject(name$).inject(changeName$).inject(interactions$);
