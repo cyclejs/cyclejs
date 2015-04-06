5bbd5efca413cabdfa6b5eb1355d10e6f7732134
Andre Medeiros <andre.medeiros@futurice.com>
2015-04-06 14:52:36 +0300

    Rewrite tests to conform with new Streams-based API

--- test/common/injection.js
+++ test/common/injection.js
@@ -1,128 +1,120 @@
 'use strict';
 /* global describe, it */
-var assert = require('assert');
-var Rx = require('rx');
-var Cycle = require('../../src/cycle');
+let assert = require('assert');
+let Cycle = require('../../src/cycle');
+let {Rx, h} = Cycle;
 
 describe('Injection', function () {
-  it('should correctly propagate data along DataFlowNodes', function (done) {
-    var dfn1 = Cycle.createDataFlowNode(function () {
-      return {m$: Rx.Observable.just(2)};
+  it('should correctly propagate data along streams', function (done) {
+    let m$ = Rx.Observable.just(2);
+    let v$ = Cycle.createStream(function (m$) {
+      return m$.map(x => x * 3);
     });
-    var dfn2 = Cycle.createDataFlowNode(function (model) {
-      return {v$: model.get('m$').map(function (x) { return x * 3; })};
+    let i$ = Cycle.createStream(function (v$) {
+      return v$.map(x => x * 5);
     });
-    var dfn3 = Cycle.createDataFlowNode(function (view) {
-      return {i$: view.get('v$').map(function (x) { return x * 5; })};
-    });
-    dfn3.get('i$').subscribe(function (x) {
+    i$.subscribe(function (x) {
       assert.strictEqual(x, 30);
       done();
     });
-    dfn3.inject(dfn2).inject(dfn1);
+    i$.inject(v$).inject(m$);
   });
 
   it('should propagate delayed intent event up until view', function (done) {
-    var fakeModel = Cycle.createDataFlowNode(function (intent) {
-      return {m$: intent.get('i$').startWith(2)};
+    let m$ = Cycle.createStream(function (i$) {
+      return i$.startWith(2);
     });
-    var fakeView = Cycle.createDataFlowNode(function (model) {
-      return {v$: model.get('m$').map(function (x) { return x * 3; })};
+    let v$ = Cycle.createStream(function (m$) {
+      return m$.map(x => x * 3);
     });
-    var fakeIntent = Cycle.createDataFlowNode(function () {
-      return {i$: Rx.Observable.just(20).delay(200)};
-    });
-    fakeView.get('v$').skip(1).subscribe(function (x) {
+    let i$ = Rx.Observable.just(20).delay(200);
+    v$.skip(1).subscribe(function (x) {
       assert.strictEqual(x, 60);
       done();
     });
-    fakeView.inject(fakeModel).inject(fakeIntent);
+    v$.inject(m$).inject(i$);
   });
 
-  it('should handle 4 DataFlowNodes in sequence', function (done) {
-    var node1 = Cycle.createDataFlowNode(function () {
-      return {one$: Rx.Observable.just(2)};
+  it('should handle 4 streams injected in sequence', function (done) {
+    let one$ = Rx.Observable.just(2);
+    let two$ = Cycle.createStream(function (one$) {
+      return one$.map(x => x * 3);
     });
-    var node2 = Cycle.createDataFlowNode(function (node1) {
-      return {two$: node1.get('one$').map(function (x) { return x * 3; })};
+    let three$ = Cycle.createStream(function (two$) {
+      return two$.map(x => x * 5);
     });
-    var node3 = Cycle.createDataFlowNode(function (node2) {
-      return {three$: node2.get('two$').map(function (x) { return x * 5; })};
+    let four$ = Cycle.createStream(function (three$) {
+      return three$.map(x => x * 7);
     });
-    var node4 = Cycle.createDataFlowNode(function (node3) {
-      return {four$: node3.get('three$').map(function (x) { return x * 7; })};
-    });
-    node4.get('four$').subscribe(function (x) {
+    four$.subscribe(function (x) {
       assert.strictEqual(x, 210);
       done();
     });
-    node4.inject(node3).inject(node2).inject(node1);
+    four$.inject(three$).inject(two$).inject(one$);
   });
 
-  it('should handle 5 DataFlowNodes in sequence', function (done) {
-    var node1 = Cycle.createDataFlowNode(function () {
-      return {one$: Rx.Observable.just(2)};
+  it('should handle 5 streams injected in sequence', function (done) {
+    let one$ = Rx.Observable.just(2);
+    let two$ = Cycle.createStream(function (one$) {
+      return one$.map(x => x * 3);
     });
-    var node2 = Cycle.createDataFlowNode(function (node1) {
-      return {two$: node1.get('one$').map(function (x) { return x * 3; })};
+    let three$ = Cycle.createStream(function (two$) {
+      return two$.map(x => x * 5);
     });
-    var node3 = Cycle.createDataFlowNode(function (node2) {
-      return {three$: node2.get('two$').map(function (x) { return x * 5; })};
+    let four$ = Cycle.createStream(function (three$) {
+      return three$.map(x => x * 7);
     });
-    var node4 = Cycle.createDataFlowNode(function (node3) {
-      return {four$: node3.get('three$').map(function (x) { return x * 7; })};
+    let five$ = Cycle.createStream(function (four$) {
+      return four$.map(x => x * 11);
     });
-    var node5 = Cycle.createDataFlowNode(function (node4) {
-      return {five$: node4.get('four$').map(function (x) { return x * 11; })};
-    });
-    node5.get('five$').subscribe(function (x) {
+    five$.subscribe(function (x) {
       assert.strictEqual(x, 2310);
       done();
     });
-    node5.inject(node4).inject(node3).inject(node2).inject(node1);
+    five$.inject(four$).inject(three$).inject(two$).inject(one$);
   });
 
-  it('should handle 6 DataFlowNodes in sequence', function (done) {
-    var node1 = Cycle.createDataFlowNode(function () {
-      return {one$: Rx.Observable.just(2)};
+  it('should handle 6 streams injected in sequence', function (done) {
+    let one$ = Rx.Observable.just(2);
+    let two$ = Cycle.createStream(function (one$) {
+      return one$.map(x => x * 3);
     });
-    var node2 = Cycle.createDataFlowNode(function (node1) {
-      return {two$: node1.get('one$').map(function (x) { return x * 3; })};
+    let three$ = Cycle.createStream(function (two$) {
+      return two$.map(x => x * 5);
     });
-    var node3 = Cycle.createDataFlowNode(function (node2) {
-      return {three$: node2.get('two$').map(function (x) { return x * 5; })};
+    let four$ = Cycle.createStream(function (three$) {
+      return three$.map(x => x * 7);
     });
-    var node4 = Cycle.createDataFlowNode(function (node3) {
-      return {four$: node3.get('three$').map(function (x) { return x * 7; })};
+    let five$ = Cycle.createStream(function (four$) {
+      return four$.map(x => x * 11);
     });
-    var node5 = Cycle.createDataFlowNode(function (node4) {
-      return {five$: node4.get('four$').map(function (x) { return x * 11; })};
+    let six$ = Cycle.createStream(function (five$) {
+      return five$.map(x => x * 13);
     });
-    var node6 = Cycle.createDataFlowNode(function (node5) {
-      return {six$: node5.get('five$').map(function (x) { return x * 13; })};
-    });
-    node6.get('six$').subscribe(function (x) {
+    six$.subscribe(function (x) {
       assert.strictEqual(x, 30030);
       done();
     });
-    node6.inject(node5).inject(node4).inject(node3).inject(node2).inject(node1);
+    six$.inject(five$).inject(four$).inject(three$).inject(two$).inject(one$);
   });
 
-  it('View data can be seen even when we subscribe after injection', function (done) {
-    var model = Cycle.createModel(function () {
-      return {m$: Rx.Observable.just(2)};
+  it('vtree$ needs replay/connect if we subscribe after injection', function (done) {
+    let m$ = Rx.Observable.just(2);
+    let vtree$ = Cycle.createStream(function (m$) {
+      // shareReplay(1) not enough, replay(null,1) and connect() needed
+      let vtree$ = m$.map(x => h('div', String(x))).replay(null, 1);
+      vtree$.connect();
+      return vtree$;
     });
-    var view = Cycle.createView(function (model) {
-      return {
-        vtree$: model.get('m$').map(function (x) { return Cycle.h('div', String(x)); }),
-        v$: model.get('m$').map(function (x) { return x * 3; })
-      };
+    let v$ = Cycle.createStream(function (m$) {
+      return m$.map(x => x * 3);
     });
-    var fakeUser = Cycle.createIntent(function (view) {
-      return {i$: view.get('v$').map(function (x) { return x * 5; })};
+    let i$ = Cycle.createStream(function (v$) {
+      return v$.map(x => x * 5);
     });
-    fakeUser.inject(view).inject(model);
-    view.get('vtree$').subscribe(function (x) {
+    i$.inject(v$).inject(m$);
+    vtree$.inject(m$);
+    vtree$.subscribe(function (x) {
       assert.strictEqual(x.type, 'VirtualNode');
       done();
     });
