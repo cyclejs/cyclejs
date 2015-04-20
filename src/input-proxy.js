1d3d0937f2ae87b090fcc94eda1c74a04919e16c
Andre Medeiros <andre.medeiros@futurice.com>
2015-04-20 18:50:21 +0300

    Refactor internals of InputProxy

--- src/input-proxy.js
+++ src/input-proxy.js
@@ -5,18 +5,18 @@ class InputProxy extends Rx.Subject {
   constructor() {
     super();
     this.type = 'InputProxy';
-    this._userEvent$ = {};
+    this._interaction$ = {};
   }
 
   // For the rendered rootElem$ with interaction$
   choose(selector, eventName) {
-    if (typeof this._userEvent$[selector] === 'undefined') {
-      this._userEvent$[selector] = {};
+    if (typeof this._interaction$[selector] === 'undefined') {
+      this._interaction$[selector] = {};
     }
-    if (typeof this._userEvent$[selector][eventName] === 'undefined') {
-      this._userEvent$[selector][eventName] = new Rx.Subject();
+    if (typeof this._interaction$[selector][eventName] === 'undefined') {
+      this._interaction$[selector][eventName] = new Rx.Subject();
     }
-    return this._userEvent$[selector][eventName];
+    return this._interaction$[selector][eventName];
   }
 }
 
