31d39b4977f936a99632b5272275e2133b65809e
Andre Medeiros <andre.medeiros@futurice.com>
2015-04-25 21:46:23 +0300

    Add docs for renderAsHTML, and improve other docs

--- rx-run/src/cycle.js
+++ rx-run/src/cycle.js
@@ -9,16 +9,16 @@ let RenderingHTML = require('./rendering/render-html');
 
 var Cycle = {
   /**
-   * Creates a Cycle stream defined by `definitionFn`. A Stream is a subclass of
+   * Creates a Cycle Stream defined by `definitionFn`. A Stream is a subclass of
    * Rx.Observable and implements "Injectable", so it contains the function
    * `inject(...inputs)`. This function will synchronously return the same Observable as
-   * `definitionFn` returns, but will use proxy inputs created internally. You should
-   * supply the real inputs later with inject(), and the proxy inputs will imitate the
-   * behavior of the real inputs.
+   * `definitionFn` returns (think IIFE: immediately-invoked function expression), but
+   * will use proxy inputs created internally. You should supply the real inputs later
+   * with inject(), and the proxy inputs will imitate the behavior of the real inputs.
    *
    * @param {Function} definitionFn a function taking Observables as input and outputting
    * one Rx.Observable.
-   * @return {Rx.Observable} a stream as defined by the return of `definitionFn`.
+   * @return {Rx.Observable} a Stream as defined by the return of `definitionFn`.
    * @function createStream
    */
   createStream: function createStream(definitionFn) {
@@ -26,25 +26,35 @@ var Cycle = {
   },
 
   /**
-   * Renders a stream of virtual DOM elements (`vtree$`) into the DOM element indicated
-   * by `container`, which can be either a CSS selector or an actual element.
-   * Returns a stream of real DOM element, with a special property attached to it called
-   * `interaction$()`. This `interaction$` is a theoretical stream containing all
+   * Renders an Observable of virtual DOM elements (`vtree$`) into the DOM element
+   * indicated by `container`, which can be either a CSS selector or an actual element.
+   * Returns an Observable of real DOM element, with a special property attached to it
+   * called `interaction$`. This `interaction$` is a theoretical Observable containing all
    * possible events happening on all elements which were rendered. You must query it
-   * with `interaction$.choose(selector, eventName)` in order to get an event stream of
+   * with `interaction$.choose(selector, eventName)` in order to get an Observable of
    * interactions of type `eventName` happening on the element identified by `selector`.
    * Example: `interaction$.choose('.mybutton', 'click').subscribe( ... )`
    *
-   * @param {Rx.Observable} vtree$ stream of virtual DOM elements.
+   * @param {Rx.Observable} vtree$ Observable of virtual DOM elements.
    * @param {(String|HTMLElement)} container the DOM selector for the element (or the
    * element itself) to contain the rendering of the VTrees.
-   * @return {Rx.Observable} a stream emitting the root DOM element for this rendering,
-   * with the property `interaction$()` attached to it.
+   * @return {Rx.Observable} an Observable emitting the root DOM element for this
+   * rendering, with the property `interaction$` attached to it.
    * @function render
    */
   render: RenderingDOM.render,
 
-  // TODO documentation
+  /**
+   * Converts a given Observable of virtual DOM elements (`vtree$`) into an Observable
+   * of corresponding HTML strings (`html$`). The provided `vtree$` must complete (must
+   * call onCompleted on its observers) in finite time, otherwise the output `html$` will
+   * never emit an HTML string.
+   *
+   * @param {Rx.Observable} vtree$ Observable of virtual DOM elements.
+   * @return {Rx.Observable} an Observable emitting a string as the HTML renderization of
+   * the virtual DOM element.
+   * @function renderAsHTML
+   */
   renderAsHTML: RenderingHTML.renderAsHTML,
 
   /**
