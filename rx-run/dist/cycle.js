d9d8ffa721adcf402d20078df298830a694d5ba0
Andre Medeiros <andre.medeiros@futurice.com>
2015-05-07 15:26:54 +0300

    Update browserify to 10.1.1

--- rx-run/dist/cycle.js
+++ rx-run/dist/cycle.js
@@ -6,32 +6,64 @@
 var process = module.exports = {};
 var queue = [];
 var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
 
 function drainQueue() {
     if (draining) {
         return;
     }
+    var timeout = setTimeout(cleanUpNextTick);
     draining = true;
-    var currentQueue;
+
     var len = queue.length;
     while(len) {
         currentQueue = queue;
         queue = [];
-        var i = -1;
-        while (++i < len) {
-            currentQueue[i]();
+        while (++queueIndex < len) {
+            currentQueue[queueIndex].run();
         }
+        queueIndex = -1;
         len = queue.length;
     }
+    currentQueue = null;
     draining = false;
+    clearTimeout(timeout);
 }
+
 process.nextTick = function (fun) {
-    queue.push(fun);
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
     if (!draining) {
         setTimeout(drainQueue, 0);
     }
 };
 
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
 process.title = 'browser';
 process.browser = true;
 process.env = {};
@@ -14210,7 +14242,7 @@ module.exports = {
   makeDestroy: makeDestroy
 };
 
-},{"./render-dom":109,"rx":58}],107:[function(require,module,exports){
+},{"./render-dom":108,"rx":58}],107:[function(require,module,exports){
 'use strict';
 var CustomElementWidget = require('./custom-element-widget');
 var Map = require('es6-map'); /* jshint: -W079 */
@@ -14264,90 +14296,6 @@ module.exports = {
 };
 
 },{"./custom-element-widget":106,"es6-map":3}],108:[function(require,module,exports){
-'use strict';
-var VirtualDOM = require('virtual-dom');
-var Rx = require('rx');
-var CustomElements = require('./custom-elements');
-var RenderingDOM = require('./render-dom');
-var RenderingHTML = require('./render-html');
-
-var Cycle = {
-  /**
-   * Takes a `computer` function which outputs an Observable of virtual DOM
-   * elements, and renders that into the DOM element indicated by `container`,
-   * which can be either a CSS selector or an actual element. At the same time,
-   * provides the `interactions` input to the `computer` function, which is a
-   * collection of all possible events happening on all elements which were
-   * rendered. You must query this collection with
-   * `interactions.get(selector, eventName)` in order to get an Observable of
-   * interactions of type `eventName` happening on the element identified by
-   * `selector`.
-   * Example: `interactions.get('.mybutton', 'click').map(ev => ...)`
-   *
-   * @param {(String|HTMLElement)} container the DOM selector for the element
-   * (or the element itself) to contain the rendering of the VTrees.
-   * @param {Function} computer a function that takes `interactions` as input
-   * and outputs an Observable of virtual DOM elements.
-   * @return {Object} an object containing properties `rootElem$`, `interactions`,
-   * `dispose()` that can be used for debugging or testing.
-   * @function applyToDOM
-   */
-  applyToDOM: RenderingDOM.applyToDOM,
-
-  /**
-   * Converts a given Observable of virtual DOM elements (`vtree$`) into an
-   * Observable of corresponding HTML strings (`html$`). The provided `vtree$`
-   * must complete (must call onCompleted on its observers) in finite time,
-   * otherwise the output `html$` will never emit an HTML string.
-   *
-   * @param {Rx.Observable} vtree$ Observable of virtual DOM elements.
-   * @return {Rx.Observable} an Observable emitting a string as the HTML
-   * renderization of the virtual DOM element.
-   * @function renderAsHTML
-   */
-  renderAsHTML: RenderingHTML.renderAsHTML,
-
-  /**
-   * Informs Cycle to recognize the given `tagName` as a custom element
-   * implemented as the given function whenever `tagName` is used in VTrees
-   * rendered in the context of some parent (in `applyToDOM` or in other custom
-   * elements).
-   * The given `definitionFn` function takes two parameters as input, in this order:
-   * `interactions` and `properties`. The former works just like it does in the
-   * `computer` function given to `applyToDOM`, and the later contains
-   * Observables representing properties of the custom element, given from the
-   * parent context. `properties.get('foo')` will return the Observable `foo$`.
-   *
-   * The `definitionFn` must output an object containing the property `vtree$`
-   * as an Observable. If the output object contains other Observables, then
-   * they are treated as custom events of the custom element.
-   *
-   * @param {String} tagName a name for identifying the custom element.
-   * @param {Function} definitionFn the implementation for the custom element.
-   * This function takes two arguments: `interactions`, and `properties`, and
-   * should output an object of Observables.
-   * @function registerCustomElement
-   */
-  registerCustomElement: CustomElements.registerCustomElement,
-
-  /**
-   * A shortcut to the root object of [RxJS](https://github.com/Reactive-Extensions/RxJS).
-   * @name Rx
-   */
-  Rx: Rx,
-
-  /**
-   * A shortcut to [virtual-hyperscript](
-   * https://github.com/Matt-Esch/virtual-dom/tree/master/virtual-hyperscript).
-   * This is a helper for creating VTrees in Views.
-   * @name h
-   */
-  h: VirtualDOM.h
-};
-
-module.exports = Cycle;
-
-},{"./custom-elements":107,"./render-dom":109,"./render-html":110,"rx":58,"virtual-dom":74}],109:[function(require,module,exports){
 /* jshint maxparams: 4 */
 'use strict';
 
@@ -14577,7 +14525,7 @@ module.exports = {
   applyToDOM: applyToDOM
 };
 
-},{"./custom-elements":107,"rx":58,"virtual-dom":74,"virtual-dom/diff":72,"virtual-dom/patch":82}],110:[function(require,module,exports){
+},{"./custom-elements":107,"rx":58,"virtual-dom":74,"virtual-dom/diff":72,"virtual-dom/patch":82}],109:[function(require,module,exports){
 'use strict';
 var Rx = require('rx');
 var toHTML = require('vdom-to-html');
@@ -14664,5 +14612,89 @@ module.exports = {
   renderAsHTML: renderAsHTML
 };
 
-},{"./custom-elements":107,"rx":58,"vdom-to-html":60}]},{},[108])(108)
+},{"./custom-elements":107,"rx":58,"vdom-to-html":60}],110:[function(require,module,exports){
+'use strict';
+var VirtualDOM = require('virtual-dom');
+var Rx = require('rx');
+var CustomElements = require('./custom-elements');
+var RenderingDOM = require('./render-dom');
+var RenderingHTML = require('./render-html');
+
+var Cycle = {
+  /**
+   * Takes a `computer` function which outputs an Observable of virtual DOM
+   * elements, and renders that into the DOM element indicated by `container`,
+   * which can be either a CSS selector or an actual element. At the same time,
+   * provides the `interactions` input to the `computer` function, which is a
+   * collection of all possible events happening on all elements which were
+   * rendered. You must query this collection with
+   * `interactions.get(selector, eventName)` in order to get an Observable of
+   * interactions of type `eventName` happening on the element identified by
+   * `selector`.
+   * Example: `interactions.get('.mybutton', 'click').map(ev => ...)`
+   *
+   * @param {(String|HTMLElement)} container the DOM selector for the element
+   * (or the element itself) to contain the rendering of the VTrees.
+   * @param {Function} computer a function that takes `interactions` as input
+   * and outputs an Observable of virtual DOM elements.
+   * @return {Object} an object containing properties `rootElem$`, `interactions`,
+   * `dispose()` that can be used for debugging or testing.
+   * @function applyToDOM
+   */
+  applyToDOM: RenderingDOM.applyToDOM,
+
+  /**
+   * Converts a given Observable of virtual DOM elements (`vtree$`) into an
+   * Observable of corresponding HTML strings (`html$`). The provided `vtree$`
+   * must complete (must call onCompleted on its observers) in finite time,
+   * otherwise the output `html$` will never emit an HTML string.
+   *
+   * @param {Rx.Observable} vtree$ Observable of virtual DOM elements.
+   * @return {Rx.Observable} an Observable emitting a string as the HTML
+   * renderization of the virtual DOM element.
+   * @function renderAsHTML
+   */
+  renderAsHTML: RenderingHTML.renderAsHTML,
+
+  /**
+   * Informs Cycle to recognize the given `tagName` as a custom element
+   * implemented as the given function whenever `tagName` is used in VTrees
+   * rendered in the context of some parent (in `applyToDOM` or in other custom
+   * elements).
+   * The given `definitionFn` function takes two parameters as input, in this order:
+   * `interactions` and `properties`. The former works just like it does in the
+   * `computer` function given to `applyToDOM`, and the later contains
+   * Observables representing properties of the custom element, given from the
+   * parent context. `properties.get('foo')` will return the Observable `foo$`.
+   *
+   * The `definitionFn` must output an object containing the property `vtree$`
+   * as an Observable. If the output object contains other Observables, then
+   * they are treated as custom events of the custom element.
+   *
+   * @param {String} tagName a name for identifying the custom element.
+   * @param {Function} definitionFn the implementation for the custom element.
+   * This function takes two arguments: `interactions`, and `properties`, and
+   * should output an object of Observables.
+   * @function registerCustomElement
+   */
+  registerCustomElement: CustomElements.registerCustomElement,
+
+  /**
+   * A shortcut to the root object of [RxJS](https://github.com/Reactive-Extensions/RxJS).
+   * @name Rx
+   */
+  Rx: Rx,
+
+  /**
+   * A shortcut to [virtual-hyperscript](
+   * https://github.com/Matt-Esch/virtual-dom/tree/master/virtual-hyperscript).
+   * This is a helper for creating VTrees in Views.
+   * @name h
+   */
+  h: VirtualDOM.h
+};
+
+module.exports = Cycle;
+
+},{"./custom-elements":107,"./render-dom":108,"./render-html":109,"rx":58,"virtual-dom":74}]},{},[110])(110)
 });
