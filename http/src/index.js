feb0cdc53280ed5daf83e79f8376a5b772306628
Andre Medeiros <andre.medeiros@futurice.com>
2015-07-04 20:53:22 +0300

    Fix code style issues in src/index.js

--- http/src/index.js
+++ http/src/index.js
@@ -4,13 +4,13 @@ let {makeHTTPDriver} = require('./http-driver');
 module.exports = {
   /**
    * HTTP Driver factory. This is a function which, when called, returns a HTTP
-   * Driver for Cycle.js apps. The driver is also a function, and it takes an 
-   * Observable of requests as input, and generates a metastream of responses. A 
-   * metastream is an Observable of Observables. The response metastream emits 
-   * Observables of responses. These Observables of responses have a `request` 
-   * field attached to them (to the Observable object itself) indicating which 
-   * request (from the driver input) generated this response Observable. The 
-   * response Observables themselves emit the response object received through 
+   * Driver for Cycle.js apps. The driver is also a function, and it takes an
+   * Observable of requests as input, and generates a metastream of responses. A
+   * metastream is an Observable of Observables. The response metastream emits
+   * Observables of responses. These Observables of responses have a `request`
+   * field attached to them (to the Observable object itself) indicating which
+   * request (from the driver input) generated this response Observable. The
+   * response Observables themselves emit the response object received through
    * superagent.
    *
    * @return {Function} the HTTP Driver function
