e7ee567e36fb0953f4cd6d35b4287c306cc57444
Andre Medeiros <andre.medeiros@futurice.com>
2015-07-08 23:52:59 +0200

    Change isomorphic example to avoid client-side rerendering
    Use .skip(1) and the Cycle.js app client-side will reuse the DOM
    elements rendered by the server. Remove .skip(1) and the client-side
    Cycle.js app will rerendering exactly the same elements as the server
    rendered as HTML. While other frameworks and their users drown in an
    ocean of tricky and tedious logic, Cycle.js yielding the Enchanted Axe
    of Reactive Extensions pwns the problem with mere 8 characters of code.
    
    Conan, what is best in life?
    https://www.youtube.com/watch?v=6PQ6335puOc

--- dom/examples/isomorphic/client.js
+++ dom/examples/isomorphic/client.js
@@ -3,7 +3,13 @@ let Cycle = require('@cycle/core');
 let CycleWeb = require('../../lib/cycle-web');
 let {app} = require('./app');
 
-Cycle.run(app, {
+function clientSideApp(responses) {
+  let requests = app(responses);
+  requests.DOM = requests.DOM.skip(1);
+  return requests;
+}
+
+Cycle.run(clientSideApp, {
   DOM: CycleWeb.makeDOMDriver('.app-container'),
   context: () => Cycle.Rx.Observable.just(window.appContext)
 });
