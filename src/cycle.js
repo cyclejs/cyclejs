6e30d457ec4812642a760f7139750b920c40fafd
Andre Medeiros <andre.medeiros@futurice.com>
2015-08-02 13:30:34 +0300

    Update ESLint to 1.0 and use new Cycle.js ESLint config

--- src/cycle.js
+++ src/cycle.js
@@ -1,91 +1,98 @@
-'use strict';
-let Rx = require('rx');
+let Rx = require(`rx`)
 
 function makeRequestProxies(drivers) {
-  let requestProxies = {};
-  for (let name in drivers) { if (drivers.hasOwnProperty(name)) {
-    requestProxies[name] = new Rx.ReplaySubject(1);
-  }}
-  return requestProxies;
+  let requestProxies = {}
+  for (let name in drivers) {
+    if (drivers.hasOwnProperty(name)) {
+      requestProxies[name] = new Rx.ReplaySubject(1)
+    }
+  }
+  return requestProxies
 }
 
 function callDrivers(drivers, requestProxies) {
-  let responses = {};
-  for (let name in drivers) { if (drivers.hasOwnProperty(name)) {
-    responses[name] = drivers[name](requestProxies[name], name);
-  }}
-  return responses;
+  let responses = {}
+  for (let name in drivers) {
+    if (drivers.hasOwnProperty(name)) {
+      responses[name] = drivers[name](requestProxies[name], name)
+    }
+  }
+  return responses
 }
 
 function makeDispose(requestProxies, rawResponses) {
   return function dispose() {
-    for (let x in requestProxies) { if (requestProxies.hasOwnProperty(x)) {
-      requestProxies[x].dispose();
-    }}
-    for (let name in rawResponses) {
-      if (rawResponses.hasOwnProperty(name) &&
-        typeof rawResponses[name].dispose === 'function')
-      {
-        rawResponses[name].dispose();
+    for (let x in requestProxies) {
+      if (requestProxies.hasOwnProperty(x)) {
+        requestProxies[x].dispose()
       }
     }
-  };
+    for (let name in rawResponses) {
+      if (rawResponses.hasOwnProperty(name) &&
+        typeof rawResponses[name].dispose === `function`)
+      {
+        rawResponses[name].dispose()
+      }
+    }
+  }
 }
 
 function makeAppInput(requestProxies, rawResponses) {
-  Object.defineProperty(rawResponses, 'dispose', {
+  Object.defineProperty(rawResponses, `dispose`, {
     enumerable: false,
-    value: makeDispose(requestProxies, rawResponses)
-  });
-  return rawResponses;
+    value: makeDispose(requestProxies, rawResponses),
+  })
+  return rawResponses
 }
 
 function logToConsoleError(err) {
-  let target = err.stack || err;
+  let target = err.stack || err
   if (console && console.error) {
-    console.error(target);
+    console.error(target)
   }
 }
 
 function replicateMany(original, imitators) {
-  for (let name in original) { if (original.hasOwnProperty(name)) {
-    if (imitators.hasOwnProperty(name) && !imitators[name].isDisposed) {
-      original[name]
-        .doOnError(logToConsoleError)
-        .subscribe(imitators[name].asObserver());
+  for (let name in original) {
+    if (original.hasOwnProperty(name)) {
+      if (imitators.hasOwnProperty(name) && !imitators[name].isDisposed) {
+        original[name]
+          .doOnError(logToConsoleError)
+          .subscribe(imitators[name].asObserver())
+      }
     }
-  }}
+  }
 }
 
 function isObjectEmpty(obj) {
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
 
 function run(main, drivers) {
-  if (typeof main !== 'function') {
-    throw new Error('First argument given to Cycle.run() must be the `main` ' +
-      'function.');
+  if (typeof main !== `function`) {
+    throw new Error(`First argument given to Cycle.run() must be the 'main' ` +
+      `function.`)
   }
-  if (typeof drivers !== 'object' || drivers === null) {
-    throw new Error('Second argument given to Cycle.run() must be an object ' +
-      'with driver functions as properties.');
+  if (typeof drivers !== `object` || drivers === null) {
+    throw new Error(`Second argument given to Cycle.run() must be an object ` +
+      `with driver functions as properties.`)
   }
   if (isObjectEmpty(drivers)) {
-    throw new Error('Second argument given to Cycle.run() must be an object ' +
-      'with at least one driver function declared as a property.');
+    throw new Error(`Second argument given to Cycle.run() must be an object ` +
+      `with at least one driver function declared as a property.`)
   }
 
-  let requestProxies = makeRequestProxies(drivers);
-  let rawResponses = callDrivers(drivers, requestProxies);
-  let responses = makeAppInput(requestProxies, rawResponses);
-  let requests = main(responses);
-  setTimeout(() => replicateMany(requests, requestProxies), 1);
-  return [requests, responses];
+  let requestProxies = makeRequestProxies(drivers)
+  let rawResponses = callDrivers(drivers, requestProxies)
+  let responses = makeAppInput(requestProxies, rawResponses)
+  let requests = main(responses)
+  setTimeout(() => replicateMany(requests, requestProxies), 1)
+  return [requests, responses]
 }
 
 let Cycle = {
@@ -115,7 +122,7 @@ let Cycle = {
    * [RxJS](https://github.com/Reactive-Extensions/RxJS).
    * @name Rx
    */
-  Rx: Rx
-};
+  Rx: Rx,
+}
 
-module.exports = Cycle;
+module.exports = Cycle
