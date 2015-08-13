94cc5c33be4416e6722642e5e13259f4d45d714a
Andre Medeiros <andre.medeiros@futurice.com>
2015-08-13 23:04:28 +0300

    Remove deprecated examples
    Check repo cyclejs/cycle-examples instead

--- dom/examples/many/many.js
+++
@@ -1,9 +0,0 @@
-function main(ext) {
-  return manyView(manyModel(manyIntent(ext)));
-}
-
-Cycle.run(main, {
-  DOM: CycleDOM.makeDOMDriver('.js-container', {
-    'many-item': manyComponent
-  })
-});
