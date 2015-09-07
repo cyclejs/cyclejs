9c91cb0cf238d55756e0aa0d0e838f479dfd71fc
Andre Medeiros <andre.medeiros@futurice.com>
2015-09-07 21:56:58 +0300

    Update counter to use DOM.select().events()

--- examples/counter/src/main.js
+++ examples/counter/src/main.js
@@ -3,8 +3,8 @@ import {h, makeDOMDriver} from '@cycle/dom';
 
 function main({DOM}) {
   let action$ = Cycle.Rx.Observable.merge(
-    DOM.get('.decrement', 'click').map(ev => -1),
-    DOM.get('.increment', 'click').map(ev => +1)
+    DOM.select('.decrement').events('click').map(ev => -1),
+    DOM.select('.increment').events('click').map(ev => +1)
   );
   let count$ = action$.startWith(0).scan((x,y) => x+y);
   return {
