6ffd89ee8c936836b62a37642d96ae9451eb18d2
Andre Medeiros <andre.medeiros@futurice.com>
2015-11-16 23:17:28 +0200

    Update counter to Cycle Nested

--- examples/counter/src/main.js
+++ examples/counter/src/main.js
@@ -1,18 +1,19 @@
+import {Observable} from 'rx';
 import Cycle from '@cycle/core';
-import {h, makeDOMDriver} from '@cycle/dom';
+import {div, button, p, makeDOMDriver} from '@cycle/dom';
 
 function main({DOM}) {
-  let action$ = Cycle.Rx.Observable.merge(
+  let action$ = Observable.merge(
     DOM.select('.decrement').events('click').map(ev => -1),
     DOM.select('.increment').events('click').map(ev => +1)
   );
   let count$ = action$.startWith(0).scan((x,y) => x+y);
   return {
     DOM: count$.map(count =>
-        h('div', [
-          h('button.decrement', 'Decrement'),
-          h('button.increment', 'Increment'),
-          h('p', 'Counter: ' + count)
+        div([
+          button('.decrement', 'Decrement'),
+          button('.increment', 'Increment'),
+          p('Counter: ' + count)
         ])
       )
   };
