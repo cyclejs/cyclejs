bed6897aea6048033aaa7a2689292c165d889a24
Andre Medeiros <andre.medeiros@futurice.com>
2016-01-24 16:29:03 +0200

    test(adapters): use test adapter and rewrite all tests

--- base/test/cycle.js
+++ base/test/cycle.js
@@ -1,62 +1,59 @@
-/* eslint-disable */
-'use strict';
-/* global describe, it */
-let assert = require('assert');
-let Cycle = require('../src/cycle');
-let Rx = require('rx');
-let sinon = require('sinon');
-let rxAdapter = require('cycle-rx-adapter')['default']
+import {describe, it} from 'mocha'
+import assert from 'assert'
+import sinon from 'sinon'
+import Cycle from '../lib'
+import * as Rx from 'rx'
+import testStreamAdapter from './test-stream-adapter';
 
 describe('Cycle', function () {
-  describe('API', function () {
-    it('should have `run`', function () {
-      assert.strictEqual(typeof Cycle.run, 'function');
-    });
+  it('should be a function', () => {
+    assert.strictEqual(typeof Cycle, 'function')
+  })
+
+  it('should throw if first argument is not a function', function () {
+    assert.throws(() => {
+      Cycle('not a function', {}, {streamAdapter: testStreamAdapter});
+    }, /First argument given to Cycle must be the 'main' function/i);
   });
 
-  describe('run()', function () {
-    it('should throw if first argument is not a function', function () {
-      assert.throws(() => {
-        Cycle.run('not a function', {}, {streamAdapter: rxAdapter});
-      }, /First argument given to Cycle\.run\(\) must be the 'main' function/i);
-    });
+  it('should throw if second argument is not an object', function () {
+    assert.throws(() => {
+      Cycle(() => {}, 'not an object', {streamAdapter: testStreamAdapter});
+    }, /Second argument given to Cycle must be an object with driver functions/i);
+  });
 
-    it('should throw if second argument is not an object', function () {
-      assert.throws(() => {
-        Cycle.run(() => {}, 'not an object', {streamAdapter: rxAdapter});
-      }, /Second argument given to Cycle\.run\(\) must be an object with driver functions/i);
-    });
+  it('should throw if second argument is an empty object', function () {
+    assert.throws(() => {
+      Cycle(() => {}, {}, {streamAdapter: testStreamAdapter});
+    }, /Second argument given to Cycle must be an object with at least one/i);
+  });
 
-    it('should throw if second argument is an empty object', function () {
-      assert.throws(() => {
-        Cycle.run(() => {}, {}, {streamAdapter: rxAdapter});
-      }, /Second argument given to Cycle\.run\(\) must be an object with at least one/i);
-    });
+  it('should throw if streamAdapter is not supplied', function () {
+    assert.throws(() => {
+      Cycle(() => {}, {other: () => {}}, {})
+    }, /Third argument given to Cycle must be an options object with the streamAdapter key supplied with a valid stream adapter/i)
+  });
 
-    it('should throw if streamAdapter is not supplied', function () {
-      assert.throws(() => {
-        Cycle.run(() => {}, {other: () => {}}, {})
-      }, /Third argument given to Cycle\.run\(\) must be an object with the streamAdapter key supplied with a valid stream adapter/i)
-    });
+  it('should return sinks object and sources object and run()', function () {
+    function app(sources) {
+      return {
+        other: sources.other.take(1).startWith('a')
+      };
+    }
+    function driver() {
+      return Rx.Observable.of('b');
+    }
+    const {sinks, sources, run} = Cycle(app, {other: driver}, {streamAdapter: testStreamAdapter});
+    assert.strictEqual(typeof sinks, 'object');
+    assert.strictEqual(typeof sinks.other.subscribe, 'function');
+    assert.strictEqual(typeof sources, 'object');
+    assert.notStrictEqual(typeof sources.other, 'undefined');
+    assert.notStrictEqual(sources.other, null);
+    assert.strictEqual(typeof sources.other.subscribe, 'function');
+    assert.strictEqual(typeof run, 'function')
+  });
 
-    it('should return sinks object and sources object', function () {
-      function app(ext) {
-        return {
-          other: ext.other.take(1).startWith('a')
-        };
-      }
-      function driver() {
-        return Rx.Observable.just('b');
-      }
-      let {sinks, sources, dispose} = Cycle.run(app, {other: driver}, {streamAdapter: rxAdapter});
-      assert.strictEqual(typeof sinks, 'object');
-      assert.strictEqual(typeof sinks.other.subscribe, 'function');
-      assert.strictEqual(typeof sources, 'object');
-      assert.notStrictEqual(typeof sources.other, 'undefined');
-      assert.notStrictEqual(sources.other, null);
-      assert.strictEqual(typeof sources.other.subscribe, 'function');
-    });
-
+  describe('run()', () => {
     it('should return a disposable drivers output', function (done) {
       function app(sources) {
         return {
@@ -66,43 +63,76 @@ describe('Cycle', function () {
       function driver(sink) {
         return sink.map(x => x.charCodeAt(0)).delay(1);
       }
-      let {sinks, sources, dispose} = Cycle.run(app, {other: driver}, {streamAdapter: rxAdapter});
+      const {sinks, sources, run} = Cycle(app, {other: driver}, {streamAdapter: testStreamAdapter});
+
       sources.other.subscribe(x => {
         assert.strictEqual(x, 97);
-        dispose();
-        done();
       });
+
+      const dispose = run();
+      dispose();
+      done();
     });
 
-    it('should happen on event loop\'s next tick', function (done) {
+    it('should allow writing one-liner drivers with default streamAdapter', function(done) {
+      function app(sources) {
+        return {}
+      }
+
+      const {sinks, sources, run} = Cycle(app, {
+        other: () => Rx.Observable.of(1, 2, 'Correct', 4),
+      }, {streamAdapter: testStreamAdapter});
+
+      sources.other.skip(2).take(1).subscribe(function(x) {
+        assert.strictEqual(typeof x, 'string');
+        assert.strictEqual(x, 'Correct');
+      });
+
+      const dispose = run();
+      setTimeout(function() {
+        dispose();
+        done();
+      }, 10)
+    });
+
+    it('should run synchronously', function () {
       function app() {
         return {
-          other: Rx.Observable.from([10, 20, 30]),
+          other: Rx.Observable.from([9, 19, 29]),
         };
       }
       let mutable = 'wrong';
       function driver(sink) {
-        return sink.map(x => 'a' + 10)
+        return sink.map(x => 'a' + 9)
       }
-      let {sinks, sources, dispose} = Cycle.run(app, {other: driver}, {streamAdapter: rxAdapter});
-      sources.other.take(1).subscribe(x => {
-        assert.strictEqual(x, 'a10');
+      const {sinks, sources, run} = Cycle(app, {other: driver}, {streamAdapter: testStreamAdapter});
+
+      let invoked = false;
+      sources.other.first().subscribe(x => {
+        assert.strictEqual(x, 'a9');
         assert.strictEqual(mutable, 'correct');
-        dispose();
-        done();
+        invoked = true;
       });
       mutable = 'correct';
+      const dispose = run();
+      dispose();
+      assert.strictEqual(invoked, true);
     });
 
     it('should not work after has been disposed', function (done) {
-      let number$ = Rx.Observable.range(1, 3)
-        .concatMap(x => Rx.Observable.just(x).delay(50));
       function app() {
-        return {other: number$};
+        return {
+          other: Rx.Observable.range(1, 3).concatMap(x =>
+            Rx.Observable.of(x).delay(50)
+          )
+        };
       }
-      let {sinks, sources, dispose} = Cycle.run(app, {
+      const {sinks, sources, run} = Cycle(app, {
         other: number$ => number$.map(number => 'x' + number)
-      }, {streamAdapter: rxAdapter});
+      }, {streamAdapter: testStreamAdapter});
+
+      const dispose = run();
+
       sources.other.subscribe(function (x) {
         assert.notStrictEqual(x, 'x3');
         if (x === 'x2') {
@@ -126,10 +156,10 @@ describe('Cycle', function () {
         };
       }
       function driver() {
-        return Rx.Observable.just('b');
+        return Rx.Observable.of('b');
       }
 
-      Cycle.run(main, {other: driver}, {streamAdapter: rxAdapter});
+      Cycle(main, {other: driver}, {streamAdapter: testStreamAdapter}).run();
       setTimeout(() => {
         sinon.assert.calledOnce(console.error);
         sinon.assert.calledWithExactly(console.error, sinon.match("malfunction"));
---
+++ base/test/test-stream-adapter.js
@@ -0,0 +1,70 @@
+import {ReplaySubject, Observable} from 'rx';
+
+function logToConsoleError(err) {
+  const target = err.stack || err;
+  if (console && console.error) {
+    console.error(target);
+  }
+}
+
+const testStreamAdapter = {
+  adapt(originStream, originStreamSubscribe) {
+    if (this.isValidStream(originStream)) {
+      return originStream;
+    }
+    const destinationStream = Observable.create(destinationObserver => {
+      const originObserver = {
+        next: x => destinationObserver.onNext(x),
+        error: e => destinationObserver.onError(e),
+        complete: () => destinationObserver.onCompleted(),
+      };
+      const dispose = originStreamSubscribe(originStream, originObserver);
+      return () => {
+        if (typeof dispose === 'function') {
+          dispose();
+        }
+      };
+    });
+    return destinationStream;
+  },
+
+  dispose(sinks, sinkProxies, sources) {
+    Object.keys(sources).forEach(k => {
+      if (typeof sources[k].dispose === 'function') {
+        sources[k].dispose();
+      }
+    });
+    Object.keys(sinkProxies).forEach(k => {
+      sinkProxies[k].observer.complete();
+    });
+  },
+
+  makeHoldSubject() {
+    const stream = new ReplaySubject(1);
+    const observer = {
+      next: x => stream.onNext(x),
+      error: e => {
+        logToConsoleError(e);
+        stream.onError(e);
+      },
+      complete: () => stream.onCompleted(),
+    };
+    return {observer, stream}
+  },
+
+  isValidStream(stream) {
+    return (
+      typeof stream.subscribe === 'function' && // should have .subscribe
+      typeof stream.onValue !== 'function'); // make sure not baconjs
+  },
+
+  streamSubscribe(stream, observer) {
+    stream.subscribe(
+      x => observer.next(x),
+      e => observer.error(e),
+      () => observer.complete()
+    );
+  },
+};
+
+export default testStreamAdapter;
