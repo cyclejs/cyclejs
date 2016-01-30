7be4dc547670900201008151be9bef48191bec32
Andre Medeiros <andre.medeiros@futurice.com>
2016-01-30 14:24:00 +0200

    Setup project structure following rxjs-adapter

--- most-adapter/src/index.ts
+++ most-adapter/src/index.ts
@@ -2,41 +2,40 @@ import {create} from 'most';
 import {holdSubject} from 'most-subject';
 
 // For TS Definitions
-import {Observer, StreamAdapter, StreamSubscriber} from '@cycle/base';
+import {Observer, HoldSubject, StreamAdapter, StreamSubscribe} from '@cycle/base';
 
 const MostAdapter: StreamAdapter = {
-  makeHoldSubject() {
-    return holdSubject();
-  },
-
-  dispose(sinks: any, sinkProxies: any, sources: any) {
-    Object.keys(sinkProxies).forEach(key => sinkProxies[key].observer.complete());
-  },
-
-  isValidStream(stream: any) {
-    if (typeof stream.observe !== 'function') {
-      return false;
-    }
-    return true;
-  },
-
-  subscribeToStream(stream: any, observer: Observer) {
-    stream.observe((x: any) => observer.next(x))
-      .then((x: any) => observer.complete(x))
-      .catch((e: Error) => observer.error(e));
-  },
-
-  adapt(originStream: any, subscribeToOriginStream: StreamSubscriber) {
+  adapt(originStream: any, originStreamSubscribe: StreamSubscribe): any {
     if (MostAdapter.isValidStream(originStream)) {
       return originStream;
     }
     return create(
       (next: (x: any) => void, complete: (x: any) => void, error: (e: any) => void) => {
         const observer = {next, complete, error};
-        subscribeToOriginStream(originStream, observer);
+        originStreamSubscribe(originStream, observer);
       }
     );
-  }
+  },
+
+  dispose(sinks: any, sinkProxies: any, sources: any) {
+    Object.keys(sinkProxies).forEach(key => sinkProxies[key].observer.complete());
+  },
+
+  makeHoldSubject(): HoldSubject {
+    return holdSubject();
+  },
+
+  isValidStream(stream: any): boolean {
+    return (
+      typeof stream.observe === 'function' &&
+      typeof stream.drain === 'function');
+  },
+
+  streamSubscribe(stream: any, observer: Observer) {
+    stream.observe((x: any) => observer.next(x))
+      .then((x: any) => observer.complete(x))
+      .catch((e: Error) => observer.error(e));
+  },
 };
 
-export default MostAdapter
+export default MostAdapter;
---
+++ most-adapter/test/index.js
@@ -0,0 +1,67 @@
+import {describe, it} from 'mocha';
+import assert from 'assert';
+import MostAdapter from '../lib';
+
+describe('MostAdapter', () => {
+  it('should conform to StreamLibrary interface', () => {
+    assert.strictEqual(typeof MostAdapter, 'object');
+    assert.strictEqual(typeof MostAdapter.adapt, 'function');
+    assert.strictEqual(typeof MostAdapter.dispose, 'function');
+    assert.strictEqual(typeof MostAdapter.makeHoldSubject, 'function');
+    assert.strictEqual(typeof MostAdapter.isValidStream, 'function');
+    assert.strictEqual(typeof MostAdapter.streamSubscribe, 'function');
+  });
+
+  // TODO
+  it('should adapt from a dummy adapter to this adapter stream', (done) => {
+    function arraySubscribe(array, observer) {
+      array.forEach(value => {
+        observer.next(value);
+      });
+      observer.complete();
+      return () => {};
+    }
+
+    const dummyStream = [1, 2, 3];
+    const mostStream = MostAdapter.adapt(dummyStream, arraySubscribe);
+    assert.strictEqual(MostAdapter.isValidStream(mostStream), true);
+    const expected = [1, 2, 3];
+    mostStream.subscribe(x => {
+      assert.strictEqual(x, expected.shift());
+    }, done.fail, () => {
+      assert.strictEqual(expected.length, 0);
+      done();
+    });
+  });
+
+  // TODO
+  it('should create a hold subject which can be fed and subscribed to', (done) => {
+    const holdSubject = MostAdapter.makeHoldSubject();
+    assert.strictEqual(holdSubject.stream instanceof ReplaySubject, true);
+    assert.strictEqual(MostAdapter.isValidStream(holdSubject.stream), true);
+
+    const observer1Expected = [1, 2, 3, 4];
+    const observer2Expected = [2, 3, 4];
+
+    MostAdapter.streamSubscribe(holdSubject.stream, {
+      next: (x) => assert.strictEqual(x, observer1Expected.shift()),
+      error: done.fail,
+      complete: () => assert.strictEqual(observer1Expected.length, 0),
+    });
+
+    holdSubject.observer.next(1);
+    holdSubject.observer.next(2);
+
+    MostAdapter.streamSubscribe(holdSubject.stream, {
+      next: (x) => assert.strictEqual(x, observer2Expected.shift()),
+      error: done.fail,
+      complete: () => assert.strictEqual(observer2Expected.length, 0),
+    });
+
+    holdSubject.observer.next(3);
+    holdSubject.observer.next(4);
+    holdSubject.observer.complete();
+
+    setTimeout(done, 20);
+  });
+});
