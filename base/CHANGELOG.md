f1e967b1d9829e9814efead30ca8b0b4654d9278
Andre Medeiros <andre.medeiros@futurice.com>
2016-01-31 23:06:50 +0200

    chore(version): v1.2.2

--- base/CHANGELOG.md
+++ base/CHANGELOG.md
@@ -1,3 +1,13 @@
+<a name="1.2.2"></a>
+## [1.2.2](https://github.com/cyclejs/base/compare/v1.2.1...v1.2.2) (2016-01-31)
+
+
+### Bug Fixes
+
+* **dispose:** add disposal of sources, if they exist ([0ac38e4](https://github.com/cyclejs/base/commit/0ac38e4))
+
+
+
 <a name="1.2.0"></a>
 # [1.2.0](https://github.com/cyclejs/base/compare/v1.2.0...v1.2.0) (2016-01-25)
 
--- base/dist/cycle-base.js
+++ base/dist/cycle-base.js
@@ -1,5 +1,5 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.CycleBase = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 var _typeof2 = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
@@ -48,6 +48,13 @@ function replicateMany(sinks, sinkProxies, streamAdapter) {
         });
     };
 }
+function disposeSources(sources) {
+    for (var k in sources) {
+        if (sources.hasOwnProperty(k) && typeof sources[k].dispose === 'function') {
+            sources[k].dispose();
+        }
+    }
+}
 var isObjectEmpty = function isObjectEmpty(obj) {
     return Object.keys(obj).length === 0;
 };
@@ -55,7 +62,7 @@ function Cycle(main, drivers, options) {
     if (typeof main !== "function") {
         throw new Error("First argument given to Cycle must be the 'main' " + "function.");
     }
-    if ((typeof drivers === "undefined" ? "undefined" : _typeof(drivers)) !== "object" || drivers === null) {
+    if ((typeof drivers === 'undefined' ? 'undefined' : _typeof(drivers)) !== "object" || drivers === null) {
         throw new Error("Second argument given to Cycle must be an object " + "with driver functions as properties.");
     }
     if (isObjectEmpty(drivers)) {
@@ -72,6 +79,7 @@ function Cycle(main, drivers, options) {
         var disposeReplication = replicateMany(sinks, sinkProxies, streamAdapter);
         return function () {
             streamAdapter.dispose(sinks, sinkProxies, sources);
+            disposeSources(sources);
             disposeReplication();
         };
     };
