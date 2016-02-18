4fc80a16afaa16c2efb8ad15164f6e59e16c01e4
Andre Medeiros <andre.medeiros@futurice.com>
2016-02-18 23:24:34 +0200

    Update http-search-github to use request categories

--- examples/http-search-github/src/main.js
+++ examples/http-search-github/src/main.js
@@ -4,24 +4,25 @@ import {div, label, input, hr, ul, li, a, makeDOMDriver} from '@cycle/dom';
 import {makeHTTPDriver} from '@cycle/http';
 
 function main(sources) {
-  const GITHUB_SEARCH_API = 'https://api.github.com/search/repositories?q=';
-
   // Requests for Github repositories happen when the input field changes,
   // debounced by 500ms, ignoring empty input field.
   const searchRequest$ = sources.DOM.select('.field').events('input')
     .debounce(500)
     .map(ev => ev.target.value)
     .filter(query => query.length > 0)
-    .map(q => GITHUB_SEARCH_API + encodeURI(q));
+    .map(q => ({
+      url: 'https://api.github.com/search/repositories?q=' + encodeURI(q),
+      category: 'github',
+    }));
 
   // Requests unrelated to the Github search. This is to demonstrate
-  // how filtering for the correct HTTP responses is necessary.
+  // how filtering for the HTTP response category is necessary.
   const otherRequest$ = Observable.interval(1000).take(2)
     .map(() => 'http://www.google.com');
 
   // Convert the stream of HTTP responses to virtual DOM elements.
   const vtree$ = sources.HTTP
-    .filter(res$ => res$.request.url.indexOf(GITHUB_SEARCH_API) === 0)
+    .filter(res$ => res$.request.category === 'github')
     .flatMap(x => x)
     .map(res => res.body.items)
     .startWith([])
