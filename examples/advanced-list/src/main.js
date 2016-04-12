8f7b0916910a867ba29f1f5ee3caee5c59fa6091
Andre Medeiros <andre.medeiros@futurice.com>
2016-04-12 15:06:46 +0300

    Convert advanced-list to Diversity

--- examples/advanced-list/src/main.js
+++ examples/advanced-list/src/main.js
@@ -1,9 +1,9 @@
-import {run} from '@cycle/core';
+import {run} from '@cycle/rx-run';
 import {makeDOMDriver} from '@cycle/dom';
 import App from './App.js';
 
 const main = App;
 
 run(main, {
-  DOM: makeDOMDriver('#main-container')
+  DOM: makeDOMDriver('#main-container', {transposition: true})
 });
