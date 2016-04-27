52eeb8f421114f9c31272cd29e0b35d36c07a269
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-04-26 21:52:44 -0400

    chore(): export interfaces for better typing from other libraries

--- history/src/index.ts
+++ history/src/index.ts
@@ -1,3 +1,5 @@
 export {makeHistoryDriver} from './makeHistoryDriver';
 export {createServerHistory} from './serverHistory';
 export {supportsHistory, createLocation} from './util';
+
+export * from './interfaces';
