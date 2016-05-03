25d008394c75ace9ce6da621a3838ab2633f9414
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-05-03 06:15:01 -0400

    feat(hyperscript-helpers): add svg helpers (#118)
    Add svg helpers to be exported by hyperscript-helpers

--- dom/src/hyperscript-helpers.ts
+++ dom/src/hyperscript-helpers.ts
@@ -28,6 +28,29 @@ function createTagFunction(tagName: string): Function {
   };
 }
 
+const SVG_TAG_NAMES = [
+  'a', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate', 'animateColor',
+  'animateMotion', 'animateTransform', 'animateTransform', 'circle', 'clipPath',
+  'color-profile', 'cursor', 'defs', 'desc', 'ellipse', 'feBlend', 'feColorMatrix',
+  'feComponentTransfer', 'feComposite', 'feConvolveMatrix', 'feDiffuseLighting',
+  'feDisplacementMap', 'feDistantLight', 'feFlood', 'feFuncA', 'feFuncB',
+  'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode',
+  'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotlight',
+  'feTile', 'feTurbulence', 'filter', 'font', 'font-face', 'font-face-format',
+  'font-face-name', 'font-face-src', 'font-face-uri', 'foreignObject', 'g',
+  'glyph', 'glyphRef', 'hkern', 'image', 'line', 'linearGradient', 'marker',
+  'mask', 'metadata', 'missing-glyph', 'mpath', 'path', 'pattern', 'polygon',
+  'polyling', 'radialGradient', 'rect', 'script', 'set', 'stop', 'style',
+  'switch', 'symbol', 'text', 'textPath', 'title', 'tref', 'tspan', 'use',
+  'view', 'vkern'
+];
+
+const svg = createTagFunction('svg');
+
+SVG_TAG_NAMES.forEach(tag => {
+  svg[tag] = createTagFunction(tag);
+});
+
 const TAG_NAMES = [
   'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base',
   'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
@@ -38,11 +61,11 @@ const TAG_NAMES = [
   'li', 'link', 'main', 'map', 'mark', 'menu', 'meta', 'nav', 'noscript',
   'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'rp', 'rt',
   'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span',
-  'strong', 'style', 'sub', 'sup', 'svg', 'table', 'tbody', 'td', 'textarea',
+  'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea',
   'tfoot', 'th', 'thead', 'title', 'tr', 'u', 'ul', 'video', 'progress'
 ];
 
-const exported = {TAG_NAMES, isSelector, createTagFunction};
+const exported = {SVG_TAG_NAMES, TAG_NAMES, svg, isSelector, createTagFunction};
 TAG_NAMES.forEach(n => {
   exported[n] = createTagFunction(n);
 });
