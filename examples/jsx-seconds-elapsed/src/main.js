67abc4b2694377166428d2f3a8951595eb030b60
Andre Staltz <andre@staltz.com>
2016-05-05 11:43:50 +0300

    Update jsx-seconds-elapsed to use xstream

--- examples/jsx-seconds-elapsed/src/main.js
+++ examples/jsx-seconds-elapsed/src/main.js
@@ -1,17 +1,17 @@
-import Cycle from '@cycle/rx-run';
-import {Observable} from 'rx';
+import xs from 'xstream';
+import Cycle from '@cycle/xstream-run';
 import {makeDOMDriver} from '@cycle/dom';
 import {html} from 'snabbdom-jsx';
 
 function main(drivers) {
   return {
-    DOM: Observable.timer(0, 1000)
+    DOM: xs.periodic(1000).map(i => i + 1).startWith(0)
       .map(i => <div>Seconds elapsed {i}</div>)
   };
 }
 
 const drivers = {
- DOM: makeDOMDriver('#main-container')
+  DOM: makeDOMDriver('#main-container')
 };
 
 Cycle.run(main, drivers);
