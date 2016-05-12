4db8e9d477e445fb9935708764fb5bd7fc5c2efc
Andre Staltz <andre@staltz.com>
2016-05-12 21:59:04 +0300

    Make it a Chrome DevTool

---
+++ devtool/src/launcher.js
@@ -0,0 +1,37 @@
+// Create a panel
+chrome.devtools.panels.create('Cycle.js', '128.png', 'panel.html', function(extensionPanel) {
+  var portToBackground = chrome.runtime.connect({name: 'cyclejs'});
+  extensionPanel.onShown.addListener(function (panelWindow) {
+    if (!panelWindow.postMessageToBackground) {
+      // Setup PANEL=>BACKGROUND communication
+      panelWindow.postMessageToBackground = function postMessageToBackground(msg) {
+        portToBackground.postMessage(msg);
+      };
+      // Setup BACKGROUND=>PANEL communication
+      portToBackground.onMessage.addListener(function (message) {
+        // alert('LAUNCHER relaying message from BACKGROUND to PANEL, message: ' + JSON.stringify(message))
+        if (message.type === 'panelData') {
+          panelWindow.postMessage({__fromCyclejsDevTool: true, data: message.data}, '*');
+        } else if (message.type === 'tabUpdated'
+        && message.tabId === chrome.devtools.inspectedWindow.tabId) {
+          chrome.devtools.inspectedWindow.eval(loadGraphSerializerCode());
+        }
+      });
+    }
+    // alert('LAUNCHER will eval the graphSerializer')
+    chrome.devtools.inspectedWindow.eval(loadGraphSerializerCode());
+  });
+});
+
+var code = null;
+
+function loadGraphSerializerCode() {
+  let xhr;
+  if (!code) {
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.extension.getURL('/graphSerializer.js'), false);
+    xhr.send();
+    code = xhr.responseText;
+  }
+  return code;
+}
