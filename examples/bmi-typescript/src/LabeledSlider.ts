b0332332387424ffdf6c678f27818e1f22f69694
Andre Staltz <andre@staltz.com>
2016-06-01 23:01:28 +0300

    Add bmi-typescript example

---
+++ examples/bmi-typescript/src/LabeledSlider.ts
@@ -0,0 +1,41 @@
+import xs, {Stream} from 'xstream';
+import {div, span, input, VNode, DOMSource} from '@cycle/dom';
+
+export interface LabeledSliderProps {
+  label: string;
+  unit: string;
+  min: number;
+  initial: number;
+  max: number;
+}
+
+export interface LabeledSliderSources {
+  props$: Stream<LabeledSliderProps>;
+  DOM: DOMSource;
+}
+
+function LabeledSlider(sources: LabeledSliderSources) {
+  let props$: Stream<LabeledSliderProps> = sources.props$;
+  let initialValue$ = props$.map(props => props.initial).take(1);
+  let newValue$ = sources.DOM.select('.slider').events('input')
+    .map((ev: Event) => (<HTMLInputElement> ev.target).value);
+  let value$: Stream<number> = initialValue$.merge(newValue$).remember();
+
+  let vtree$ = xs.combine(
+    (props, value) =>
+      div('.labeled-slider', [
+        span('.label', [ props.label + ' ' + value + props.unit ]),
+        input('.slider', {
+          attrs: {type: 'range', min: props.min, max: props.max, value: value}
+        })
+      ]),
+    props$, value$
+  );
+
+  return {
+    DOM: vtree$,
+    value$: value$,
+  };
+}
+
+export default LabeledSlider;
