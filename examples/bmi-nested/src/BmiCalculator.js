c77c20caceaf4cf21e090ac72c6fc1b504832939
Andre Staltz <andre@staltz.com>
2016-06-03 18:51:20 +0300

    Update packages in bmi-nested example

--- examples/bmi-nested/src/BmiCalculator.js
+++ examples/bmi-nested/src/BmiCalculator.js
@@ -27,14 +27,14 @@ function BmiCalculator({DOM}) {
   );
 
   return {
-    DOM: bmi$.combine(
+    DOM: xs.combine(
       (bmi, weightVTree, heightVTree) =>
         div([
           weightVTree,
           heightVTree,
           h2('BMI is ' + bmi)
         ]),
-      weightSlider.DOM, heightSlider.DOM
+      bmi$, weightSlider.DOM, heightSlider.DOM
     )
   };
 }
--- examples/bmi-nested/src/LabeledSlider.js
+++ examples/bmi-nested/src/LabeledSlider.js
@@ -4,7 +4,7 @@ import {div, span, input} from '@cycle/dom';
 function LabeledSlider({DOM, props$}) {
   let initialValue$ = props$.map(props => props.initial).take(1);
   let newValue$ = DOM.select('.slider').events('input').map(ev => ev.target.value);
-  let value$ = initialValue$.merge(newValue$).remember();
+  let value$ = xs.merge(initialValue$, newValue$).remember();
 
   let vtree$ = xs.combine(
     (props, value) =>
