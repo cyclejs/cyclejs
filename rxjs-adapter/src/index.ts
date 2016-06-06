eca5030992a6a519d795e6f49fa947c2f651ea40
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-06 18:01:15 -0400

    feat(src): update to latest adapter interface
    Remove dispose()
    Add remember()
    
    BREAKING CHANGES:
     removes dispose()

--- rxjs-adapter/src/index.ts
+++ rxjs-adapter/src/index.ts
@@ -1,36 +1,18 @@
 import {
   StreamAdapter,
   Observer,
-  SinkProxies,
   StreamSubscribe,
   DisposeFunction,
   Subject,
 } from '@cycle/base';
 import * as Rx from 'rxjs';
 
-function logToConsoleError(err: any) {
-  const target = err.stack || err;
-  if (console && console.error) {
-    console.error(target);
-  } else if (console && console.log) {
-    console.log(target);
-  }
-}
-
-function attemptSubjectComplete<T>(subject: Rx.Subject<T>): void {
-  try {
-    subject.complete();
-  } catch (err) {
-    return void 0;
-  }
-}
-
 const RxJSAdapter: StreamAdapter = {
   adapt<T>(originStream: any, originStreamSubscribe: StreamSubscribe): Rx.Observable<T> {
     if (this.isValidStream(originStream)) {
       return originStream;
     }
-    return Rx.Observable.create((observer: Observer) => {
+    return Rx.Observable.create((observer: Observer<T>) => {
       const dispose = originStreamSubscribe(originStream, observer);
       return () => {
         if (typeof dispose === 'function') {
@@ -40,25 +22,15 @@ const RxJSAdapter: StreamAdapter = {
     });
   },
 
-  dispose(sinks: any, sinkProxies: SinkProxies, sources: any) {
-    Object.keys(sources).forEach(k => {
-      if (typeof sources[k].unsubscribe === 'function') {
-        sources[k].unsubscribe();
-      }
-    });
-    Object.keys(sinkProxies).forEach(k => {
-      attemptSubjectComplete(<Rx.Subject<any>> sinkProxies[k].observer);
-    });
+  remember<T>(observable: Rx.Observable<T>): Rx.Observable<T> {
+    return observable.publishReplay(1);
   },
 
-  makeSubject(): Subject {
-    const stream: Rx.Subject<any> = new Rx.Subject();
-    const observer: Rx.Observer<any> = {
-      next: x => { stream.next(x); },
-      error: err => {
-        logToConsoleError(err);
-        stream.error(err);
-      },
+  makeSubject<T>(): Subject<T> {
+    const stream: Rx.Subject<T> = new Rx.Subject<T>();
+    const observer: Rx.Observer<T> = {
+      next: (x: T) => { stream.next(x); },
+      error: (err: any) => { stream.error(err); },
       complete: () => { stream.complete(); },
     };
     return {stream, observer};
@@ -71,8 +43,8 @@ const RxJSAdapter: StreamAdapter = {
       typeof stream.onValue !== 'function');
   },
 
-  streamSubscribe(stream: Rx.Observable<any>, observer: Observer) {
-    const subscription = stream.subscribe(<any> observer);
+  streamSubscribe <T>(stream: Rx.Observable<T>, observer: Observer<T>) {
+    const subscription = stream.subscribe(observer);
     return () => {
       subscription.unsubscribe();
     };
