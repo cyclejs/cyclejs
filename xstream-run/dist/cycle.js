ecebba5c10949b1c332809d8b222c889c2ce1e49
Andre Staltz <andre@staltz.com>
2016-06-08 17:59:24 +0300

    chore(dist): build dist/

--- xstream-run/dist/cycle.js
+++ xstream-run/dist/cycle.js
@@ -74,6 +74,14 @@ exports.default = Cycle;
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
+function logToConsoleError(err) {
+    var target = err.stack || err;
+    if (console && console.error) {
+        console.error(target);
+    } else if (console && console.log) {
+        console.log(target);
+    }
+}
 function makeSinkProxies(drivers, streamAdapter) {
     var sinkProxies = {};
     for (var name_1 in drivers) {
@@ -108,7 +116,18 @@ function replicateMany(sinks, sinkProxies, streamAdapter) {
     var results = Object.keys(sinks).filter(function (name) {
         return !!sinkProxies[name];
     }).map(function (name) {
-        return streamAdapter.streamSubscribe(sinks[name], sinkProxies[name].observer);
+        return streamAdapter.streamSubscribe(sinks[name], {
+            next: function next(x) {
+                sinkProxies[name].observer.next(x);
+            },
+            error: function error(err) {
+                logToConsoleError(err);
+                sinkProxies[name].observer.error(err);
+            },
+            complete: function complete(x) {
+                sinkProxies[name].observer.complete(x);
+            }
+        });
     });
     var disposeFunctions = results.filter(function (dispose) {
         return typeof dispose === 'function';
@@ -152,7 +171,6 @@ function Cycle(main, drivers, options) {
     var run = function run() {
         var disposeReplication = replicateMany(sinks, sinkProxies, streamAdapter);
         return function () {
-            streamAdapter.dispose(sinks, sinkProxies, sources);
             disposeSources(sources);
             disposeReplication();
         };
@@ -166,15 +184,6 @@ exports.default = Cycle;
 },{}],3:[function(require,module,exports){
 "use strict";
 var xstream_1 = require('xstream');
-function logToConsoleError(err) {
-    var target = err.stack || err;
-    if (console && console.error) {
-        console.error(target);
-    }
-    else if (console && console.log) {
-        console.log(target);
-    }
-}
 var XStreamAdapter = {
     adapt: function (originStream, originStreamSubscribe) {
         if (XStreamAdapter.isValidStream(originStream)) {
@@ -184,11 +193,7 @@ var XStreamAdapter = {
         var dispose = null;
         return xstream_1.default.create({
             start: function (out) {
-                var observer = {
-                    next: function (value) { return out.shamefullySendNext(value); },
-                    error: function (err) { return out.shamefullySendError(err); },
-                    complete: function () { return out.shamefullySendComplete(); },
-                };
+                var observer = out;
                 dispose = originStreamSubscribe(originStream, observer);
             },
             stop: function () {
@@ -198,27 +203,17 @@ var XStreamAdapter = {
             }
         });
     },
-    dispose: function (sinks, sinkProxies, sources) {
-        Object.keys(sources).forEach(function (k) {
-            if (typeof sources[k].dispose === 'function') {
-                sources[k].dispose();
-            }
-        });
-        Object.keys(sinks).forEach(function (k) {
-            sinks[k].removeListener(sinkProxies[k].stream);
-        });
-    },
     makeSubject: function () {
         var stream = xstream_1.default.create();
         var observer = {
             next: function (x) { stream.shamefullySendNext(x); },
-            error: function (err) {
-                logToConsoleError(err);
-                stream.shamefullySendError(err);
-            },
+            error: function (err) { stream.shamefullySendError(err); },
             complete: function () { stream.shamefullySendComplete(); }
         };
         return { observer: observer, stream: stream };
+    },
+    remember: function (stream) {
+        return stream.remember();
     },
     isValidStream: function (stream) {
         return (typeof stream.addListener === 'function' &&
