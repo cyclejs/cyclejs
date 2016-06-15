f565e5193ed03283efc08be78c1628be2f89b267
Andre Staltz <andre@staltz.com>
2016-06-15 11:15:37 +0300

    Build dist/

--- isolate/dist/cycle-isolate.js
+++ isolate/dist/cycle-isolate.js
@@ -9,8 +9,9 @@ function checkIsolateArgs(dataflowComponent, scope) {
         throw new Error("First argument given to isolate() must be a " +
             "'dataflowComponent' function");
     }
-    if (scope === null) {
-        throw new Error("Second argument given to isolate() must not be null");
+    if (typeof scope !== "string") {
+        throw new Error("Second argument given to isolate() must be a " +
+            "string for 'scope'");
     }
 }
 function isolateAllSources(sources, scope) {
@@ -73,15 +74,14 @@ function isolateAllSinks(sources, sinks, scope) {
 function isolate(component, scope) {
     if (scope === void 0) { scope = newScope(); }
     checkIsolateArgs(component, scope);
-    var convertedScope = typeof scope === 'string' ? scope : scope.toString();
     return function scopedComponent(sources) {
         var rest = [];
         for (var _i = 1; _i < arguments.length; _i++) {
             rest[_i - 1] = arguments[_i];
         }
-        var scopedSources = isolateAllSources(sources, convertedScope);
+        var scopedSources = isolateAllSources(sources, scope);
         var sinks = component.apply(void 0, [scopedSources].concat(rest));
-        var scopedSinks = isolateAllSinks(sources, sinks, convertedScope);
+        var scopedSinks = isolateAllSinks(sources, sinks, scope);
         return scopedSinks;
     };
 }
