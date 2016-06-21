5aff724bb3aae65139ea931d5a1ac0a1144968ce
Andre Staltz <andre@staltz.com>
2016-06-22 00:07:41 +0300

    docs(api): improve API.md for Diversity release

--- jsonp/src/index.js
+++ jsonp/src/index.js
@@ -44,18 +44,18 @@ module.exports = {
    * JSONP Driver factory.
    *
    * This is a function which, when called, returns a JSONP Driver for Cycle.js
-   * apps. The driver is also a function, and it takes an Observable of requests
+   * apps. The driver is also a function, and it takes a stream of requests
    * (URL strings) as input, and generates a metastream of responses.
    *
-   * **Requests**. The Observable of requests should strings as the URL of the
+   * **Requests**. The stream of requests should emit strings as the URL of the
    * remote resource over HTTP.
    *
-   * **Responses**. A metastream is an Observable of Observables. The response
-   * metastream emits Observables of responses. These Observables of responses
-   * have a `request` field attached to them (to the Observable object itself)
-   * indicating which request (from the driver input) generated this response
-   * Observable. The response Observables themselves emit the response object
-   * received through the npm `jsonp` package.
+   * **Responses**. A metastream is a stream of streams. The response metastream
+   * emits streams of responses. These streams of responses have a `request`
+   * field attached to them (to the stream object itself) indicating which
+   * request (from the driver input) generated this response stream. The
+   * response streams themselves emit the response object received through the
+   * npm `jsonp` package.
    *
    * @return {Function} the JSONP Driver function
    * @function makeJSONPDriver
