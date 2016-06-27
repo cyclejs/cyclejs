c279d94aec032ca3a27b88bef4f387d427572681
Andre Staltz <andre@staltz.com>
2016-06-27 16:52:47 +0300

    Add rx-typings.d.ts for HTTPSource

---
+++ http/rx-typings.d.ts
@@ -0,0 +1,7 @@
+import {ResponseStream, Response} from './lib/interfaces';
+import {Observable} from 'rx';
+export interface HTTPSource {
+  response$$: Observable<Observable<Response> & ResponseStream>;
+  filter(predicate: (response$: ResponseStream & Observable<Response>) => boolean): HTTPSource;
+  select(category: string): Observable<Observable<Response> & ResponseStream>;
+}
