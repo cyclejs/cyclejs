2db0fa258c5e410a66f77a9e6dda7c26aaec68f7
Max Stoiber <contact@mxstbr.com>
2016-07-02 17:02:15 +0200

    Change main function variable names (#32)

--- examples/jsx-seconds-elapsed/src/main.js
+++ examples/jsx-seconds-elapsed/src/main.js
@@ -3,7 +3,7 @@ import Cycle from '@cycle/xstream-run';
 import {makeDOMDriver} from '@cycle/dom';
 import {html} from 'snabbdom-jsx';
 
-function main(drivers) {
+function main(sources) {
   return {
     DOM: xs.periodic(1000).map(i => i + 1).startWith(0)
       .map(i => <div>Seconds elapsed {i}</div>)
