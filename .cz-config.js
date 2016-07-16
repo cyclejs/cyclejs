2ec2382409e705a49587999f8bb5fae57aa5e540
Andre Staltz <andre@staltz.com>
2016-07-17 00:19:45 +0300

    chore(META): introduce new commit type: 'release'

--- .cz-config.js
+++ .cz-config.js
@@ -7,6 +7,7 @@ module.exports = {
     {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
     {value: 'test',     name: 'test:     Add tests only'},
     {value: 'docs',     name: 'docs:     Documentation only changes'},
+    {value: 'release',  name: 'release:  Publish a new version of a package.'},
     {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation. META only.'},
     {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
     {value: 'perf',     name: 'perf:     A code change that improves performance'},
