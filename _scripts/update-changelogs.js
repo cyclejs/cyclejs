6c1e8327861f5629f45494a4e855cfa55937025c
Andre Staltz <andre@staltz.com>
2016-07-18 16:11:37 +0300

    chore(META): add changelog script for each package
    Add new npm run script to generate changelog for a particular package,
    using the top-level update-changelogs.js script. Also run this script
    when releasing a new version of a package.

--- _scripts/update-changelogs.js
+++ _scripts/update-changelogs.js
@@ -12,21 +12,32 @@ var packagesWithChangelog = fs
   .trim()
   .split('\n');
 
-packagesWithChangelog.forEach(function (package) {
-  var writeStream = fs.createWriteStream('./' + package + '/CHANGELOG.md')
-  conventionalChangelog({
-    preset: 'angular',
-    pkg: {
-      path: './' + package + '/package.json',
-    },
-    transform: function (commit, cb) {
-      if (commit.scope === package) {
-        console.log(commit);
-        cb(null, commit);
-      } else {
-        cb();
-      }
-    },
-  }, {}, {from: theCommitThatStartedTheMonorepo})
-    .pipe(writeStream); // or any writable strea
-});
+var argPackage = process.argv[2];
+
+packagesWithChangelog
+  .filter(function (package) {
+    if (typeof argPackage === 'string' && argPackage.length > 0) {
+      return argPackage === package;
+    } else {
+      return true;
+    }
+  })
+  .forEach(function (package) {
+    console.log('updating changelog for package ' + package);
+    var writeStream = fs.createWriteStream(__dirname + '/../' + package + '/CHANGELOG.md')
+    conventionalChangelog({
+      preset: 'angular',
+      pkg: {
+        path: __dirname + '/../' + package + '/package.json',
+      },
+      transform: function (commit, cb) {
+        if (commit.scope === package) {
+          console.log(commit);
+          cb(null, commit);
+        } else {
+          cb();
+        }
+      },
+    }, {}, {from: theCommitThatStartedTheMonorepo})
+      .pipe(writeStream); // or any writable strea
+  });
