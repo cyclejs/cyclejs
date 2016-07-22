445d5fc9b2fa3268b8651e4ba2e0ce1e8cab1a73
Andre Staltz <andre@staltz.com>
2016-07-22 13:27:54 +0300

    chore(META): fix update-changelog script to consider old versions

--- _scripts/update-changelogs.js
+++ _scripts/update-changelogs.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var conventionalChangelog = require('conventional-changelog');
+var addStream = require('add-stream');
+var tempfile = require('tempfile');
 var fs = require('fs');
 
 var theCommitThatStartedTheMonorepo = fs
@@ -14,30 +16,63 @@ var packagesWithChangelog = fs
 
 var argPackage = process.argv[2];
 
-packagesWithChangelog
-  .filter(function (package) {
-    if (typeof argPackage === 'string' && argPackage.length > 0) {
-      return argPackage === package;
-    } else {
-      return true;
+// Assume that for each package we will start iterating from
+// theCommitThatStartedTheMonorepo onwards.
+var startCommits = {};
+packagesWithChangelog.forEach(function (package) {
+  startCommits[package] = theCommitThatStartedTheMonorepo;
+});
+
+// Update the startCommit for each package, looking for release commits
+// for each package.
+conventionalChangelog({
+  preset: 'angular',
+  append: true,
+  transform: function (commit, cb) {
+    if (commit.type === 'release') {
+      startCommits[commit.scope] = commit.hash;
     }
-  })
-  .forEach(function (package) {
-    console.log('updating changelog for package ' + package);
-    var writeStream = fs.createWriteStream(__dirname + '/../' + package + '/CHANGELOG.md')
-    conventionalChangelog({
-      preset: 'angular',
-      pkg: {
-        path: __dirname + '/../' + package + '/package.json',
-      },
-      transform: function (commit, cb) {
-        if (commit.scope === package) {
-          console.log(commit);
-          cb(null, commit);
-        } else {
-          cb();
-        }
-      },
-    }, {}, {from: theCommitThatStartedTheMonorepo})
-      .pipe(writeStream); // or any writable strea
-  });
+    cb();
+  }
+}, {}, { from: theCommitThatStartedTheMonorepo, reverse: true })
+  .on('end', runUpdateChangelogs).resume();
+
+function runUpdateChangelogs() {
+  packagesWithChangelog
+    .filter(function (package) {
+      if (typeof argPackage === 'string' && argPackage.length > 0) {
+        return argPackage === package;
+      } else {
+        return true;
+      }
+    })
+    .forEach(function (package) {
+      console.log('updating changelog for package ' + package);
+      var filename = __dirname + '/../' + package + '/CHANGELOG.md';
+      var changelogOpts = {
+        preset: 'angular',
+        releaseCount: 0,
+        pkg: {
+          path: __dirname + '/../' + package + '/package.json',
+        },
+        transform: function (commit, cb) {
+          if (commit.scope === package) {
+            cb(null, commit);
+          } else {
+            cb();
+          }
+        },
+      };
+      var gitRawCommitsOpts = { from: startCommits[package] };
+
+      var readStream = fs.createReadStream(filename);
+      var tmp = tempfile();
+      conventionalChangelog(changelogOpts, {}, gitRawCommitsOpts)
+        .pipe(addStream(readStream))
+        .pipe(fs.createWriteStream(tmp))
+        .on('finish', function () {
+          fs.createReadStream(tmp)
+            .pipe(fs.createWriteStream(filename));
+        });
+    })
+}
