1bf5244ae4ee03cb4da1649941b43a2e0cc4ddaa
Max Battcher <maxwell.battcher@stantec.com>
2016-08-25 10:30:00 -0400

    feat(dom): add Snabbdom dataset module to the defaults
    The dataset module is a small, useful module that provides an easier, more straightforward, way to
    
    attach data information to an element. It is generally useful enough to be a good default for
    
    everyone.
    
    ISSUES CLOSED: #396

--- dom/src/modules.ts
+++ dom/src/modules.ts
@@ -2,14 +2,16 @@ declare function require(s: string): any;
 const ClassModule = require('snabbdom/modules/class');
 const PropsModule = require('snabbdom/modules/props');
 const AttrsModule = require('snabbdom/modules/attributes');
+const DatasetModule = require('snabbdom/modules/dataset');
 const EventsModule = require('snabbdom/modules/eventlisteners');
 const StyleModule = require('snabbdom/modules/style');
 const HeroModule = require('snabbdom/modules/hero');
 
-export default [StyleModule, ClassModule, PropsModule, AttrsModule];
+export default [StyleModule, ClassModule, PropsModule, AttrsModule, DatasetModule];
 
 export {
   StyleModule, ClassModule,
   PropsModule, AttrsModule,
   HeroModule, EventsModule,
+  DatasetModule,
 }
