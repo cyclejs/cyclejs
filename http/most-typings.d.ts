d1d70aaac78eb0bfa08c1b72bcae82f6baa2a7ca
whitecolor <alex.oshchepkov@gmail.com>
2016-09-03 19:46:29 +0500

    fix(http): fix typings after removal of response$$
    Remove response$$ from interfaces and fix filter and select signatures

--- http/most-typings.d.ts
+++ http/most-typings.d.ts
@@ -1,7 +1,6 @@
-import {ResponseStream, Response} from './lib/interfaces';
+import {ResponseStream, Response, RequestOptions} from './lib/interfaces';
 import {Stream} from 'most';
-export interface HTTPSource {
-  response$$: Stream<Stream<Response> & ResponseStream>;
-  filter(predicate: (response$: ResponseStream & Stream<Response>) => boolean): HTTPSource;
-  select(category: string): Stream<Stream<Response> & ResponseStream>;
+export interface HTTPSource {  
+  filter(predicate: (request: RequestOptions) => boolean): HTTPSource;
+  select(category?: string): Stream<Stream<Response> & ResponseStream>;
 }
