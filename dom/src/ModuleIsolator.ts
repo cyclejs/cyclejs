10fc5bd30f039140182968272600c0ecef290638
Frederik Krautwald <fkrautwald@gmail.com>
2016-09-28 17:20:10 +0200

    refactor(dom): improve readability

--- dom/src/ModuleIsolator.ts
+++ dom/src/ModuleIsolator.ts
@@ -3,8 +3,55 @@ import {EventDelegator} from './EventDelegator';
 let MapPolyfill: typeof Map = require('es6-map');
 
 export class ModuleIsolator {
+
   private eventDelegators = new MapPolyfill<string, Array<EventDelegator>>();
-  constructor (private isolatedElements: Map<string, Element>) {
+
+  constructor (private isolatedElements: Map<string, Element>) {}
+
+  createModule() {
+    const self = this;
+
+    return {
+      create(oldVNode: VNode, vNode: VNode) {
+        const {element, oldScope, scope} = ModuleIsolator.extractElementAndScopesFromVNodes(oldVNode, vNode);
+        const eventDelegators: Array<EventDelegator> = self.eventDelegators.get(scope);
+        if (scope) { self.setScope(<Element> element, scope); }
+        if (oldScope) { self.removeScope(oldScope); }
+        if (scope && eventDelegators) {
+          for (let i in eventDelegators) {
+            eventDelegators[i].updateTopElement(<Element> element);
+          }
+          return;
+        }
+        const isScopeWithoutEventDelegators = scope && eventDelegators === void 0;
+        if (isScopeWithoutEventDelegators) { self.eventDelegators.set(scope, []); }
+      },
+
+      update(oldVNode: VNode, vNode: VNode) {
+        const {element, oldScope, scope} = ModuleIsolator.extractElementAndScopesFromVNodes(oldVNode, vNode);
+        if (oldScope && scope && scope !== oldScope) { self.removeScope(oldScope); }
+        if (scope && scope !== oldScope) { self.setScope(<Element> element, scope); }
+        if (oldScope && !scope) { self.removeScope(scope); }
+      },
+
+      remove(vNode: VNode, cb: Function) {
+        self.cleanUpVNode(vNode);
+        cb();
+      },
+
+      destroy(vNode: VNode) {
+        self.cleanUpVNode(vNode);
+      }
+    };
+  }
+
+  static extractElementAndScopesFromVNodes(oldVNode: VNode, vNode: VNode) {
+    const {data: oldData = {}} = oldVNode;
+    const {elm: element, data = {}} = vNode;
+    const oldScope: string = oldData.isolate || ``;
+    const scope: string = data.isolate || ``;
+
+    return {element, oldScope, scope};
   }
 
   private setScope(elm: Element, scope: string) {
@@ -15,15 +62,15 @@ export class ModuleIsolator {
     this.isolatedElements.delete(scope);
   }
 
-  private cleanupVNode({data, elm}: VNode) {
-    data = data || {};
+  private cleanUpVNode({elm: element, data = {}}: VNode) {
     const scope = (<any> data).isolate;
-    const isCurrentElm = this.isolatedElements.get(scope) === elm;
-    if (scope && isCurrentElm) {
+    const isCurrentElement = this.isolatedElements.get(scope) === element;
+    const isScopeAndCurrentElement = scope && isCurrentElement;
+    if (isScopeAndCurrentElement) {
       this.removeScope(scope);
-      if (this.eventDelegators.get(scope)) {
-        this.eventDelegators.set(scope, []);
-      }
+    }
+    if (isScopeAndCurrentElement && this.eventDelegators.get(scope)) {
+      this.eventDelegators.set(scope, []);
     }
   }
 
@@ -31,79 +78,26 @@ export class ModuleIsolator {
     return this.isolatedElements.get(scope);
   }
 
-  isIsolatedElement(elm: Element): string | boolean {
-    let iterator = this.isolatedElements.entries();
+  isIsolatedElement(subject: Element): string | boolean {
+    const iterator = this.isolatedElements.entries();
     for (let result = iterator.next(); !!result.value; result = iterator.next()) {
       const [scope, element] = result.value;
-      if (elm === element) {
-        return scope;
-      }
+      if (subject === element) { return scope; }
     }
+
     return false;
   }
 
   addEventDelegator(scope: string, eventDelegator: EventDelegator) {
-    let delegators = this.eventDelegators.get(scope);
-    if (!delegators) {
-      delegators = [];
-      this.eventDelegators.set(scope, delegators);
+    let eventDelegators = this.eventDelegators.get(scope);
+    if (!eventDelegators) {
+      eventDelegators = [];
+      this.eventDelegators.set(scope, eventDelegators);
     }
-    delegators[delegators.length] = eventDelegator;
+    eventDelegators[eventDelegators.length] = eventDelegator;
   }
 
   reset() {
     this.isolatedElements.clear();
   }
-
-  createModule() {
-    const self = this;
-    return {
-      create(oldVNode: VNode, vNode: VNode) {
-        const {data: oldData = {}} = oldVNode;
-        const {elm, data = {}} = vNode;
-        const oldScope = oldData.isolate || ``;
-        const scope = data.isolate || ``;
-        if (scope) {
-          if (oldScope) { self.removeScope(oldScope); }
-          self.setScope(<Element> elm, scope);
-          const delegators = self.eventDelegators.get(scope);
-          if (delegators) {
-            for (let i = 0, len = delegators.length; i < len; ++i) {
-              delegators[i].updateTopElement(<Element> elm);
-            }
-          } else if (delegators === void 0) {
-            self.eventDelegators.set(scope, []);
-          }
-        }
-        if (oldScope && !scope) {
-          self.removeScope(scope);
-        }
-      },
-
-      update(oldVNode: VNode, vNode: VNode) {
-        const {data: oldData = {}} = oldVNode;
-        const {elm, data = {}} = vNode;
-        const oldScope = oldData.isolate || ``;
-        const scope = data.isolate || ``;
-        if (scope && scope !== oldScope) {
-          if (oldScope) { self.removeScope(oldScope); }
-          self.setScope(<Element> elm, scope);
-        }
-        if (oldScope && !scope) {
-          self.removeScope(scope);
-        }
-      },
-
-      remove(vNode: VNode, cb: Function) {
-        self.cleanupVNode(vNode);
-        cb();
-      },
-
-      destroy(vNode: VNode) {
-        self.cleanupVNode(vNode);
-      }
-    };
-  }
-
-  // snabbdom module stuff
 }
