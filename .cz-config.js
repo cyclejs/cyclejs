5acde1fa5308b5ae61d22fbda036db6f3e42c3da
Andre Staltz <andre@staltz.com>
2016-10-13 20:37:29 +0300

    chore(META): improve scope choices for npm run commit

--- .cz-config.js
+++ .cz-config.js
@@ -1,42 +1,52 @@
 'use strict';
 
+var packageScopes = [
+  'base',
+  'devtool',
+  'dom',
+  'history',
+  'http',
+  'isolate',
+  'jsonp',
+  'most-adapter',
+  'most-run',
+  'rx-adapter',
+  'rx-run',
+  'rxjs-adapter',
+  'rxjs-run',
+  'xstream-adapter',
+  'xstream-run'
+];
+
+var otherScopes = [
+  'META',
+  'examples'
+];
+
 module.exports = {
   types: [
     {value: 'feat',     name: 'feat:     Add a new feature'},
     {value: 'fix',      name: 'fix:      Submit a bug fix'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature.\n            Includes code style changes.'},
     {value: 'test',     name: 'test:     Add tests only'},
     {value: 'docs',     name: 'docs:     Documentation only changes'},
     {value: 'release',  name: 'release:  Publish a new version of a package.'},
     {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation. META only.'},
-    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
     {value: 'perf',     name: 'perf:     A code change that improves performance'},
   ],
 
-  scopes: [
-    {name: 'META'},
-    {name: 'base'},
-    {name: 'devtool'},
-    {name: 'dom'},
-    {name: 'examples'},
-    {name: 'history'},
-    {name: 'http'},
-    {name: 'isolate'},
-    {name: 'jsonp'},
-    {name: 'most-adapter'},
-    {name: 'most-run'},
-    {name: 'rx-adapter'},
-    {name: 'rx-run'},
-    {name: 'rxjs-adapter'},
-    {name: 'rxjs-run'},
-    {name: 'xstream-adapter'},
-    {name: 'xstream-run'},
-  ],
+  scopes: packageScopes.concat(otherScopes)
+    .sort()
+    .map(name => ({name})),
 
   scopeOverrides: {
     chore: [
       {name: 'META'},
-    ]
+    ],
+    feat: packageScopes,
+    fix: packageScopes,
+    release: packageScopes,
+    test: packageScopes,
   },
 
   allowCustomScopes: false,
