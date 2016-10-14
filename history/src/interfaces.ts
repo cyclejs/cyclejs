cyclejs/cyclejs

commit 2d3bb3c3a6634842a659e1d22b02ea8f25a83050

refactor(history): use tsconfig strictNullChecks

history/src/serverHistory.ts
@@ -51,6 +51,6 @@ class ServerHistory implements History {
   }
 }
 
-export function createServerHistory(loc?: Location | Pathname): History {
-  return new ServerHistory(loc ? createLocation(loc) : null);
+export function createServerHistory(loc: Location | Pathname | undefined): History {
+  return new ServerHistory(createLocation(loc));
 }

commit f4c3da8939a04ae5ef841a6436d361741f759112

refactor(history): fix/undo broken code from 2d3bb3c
If we had released history driver with commit 2d3bb3c, that would be a bug since the tests were not

passing. Because we did not release, this commit undoes the bugs that 2d3bb3c introduced, so +1 -1

means there is no new release that needs to be done, and in total, we just refactored code.

history/src/interfaces.ts
@@ -27,7 +27,7 @@ export interface History {
   replace(location: Location| Pathname): void;
   createHref(href: Pathname): string;
   createLocation(location: Location | Pathname): Location;
-  getCurrentLocation(): Location;
+  getCurrentLocation(): Location | null;
   addCompleteCallback?: (fn: () => void) => void;
   complete?: () => void;
 };

history/src/makeHistoryDriver.ts
@@ -5,9 +5,9 @@ import {captureClicks} from './captureClicks';
 
 function makeUpdateHistory(history: History) {
   return function updateHistory(location: Location | Pathname) {
-    if ('string' === typeof location) {
+    if (typeof location === 'string') {
       history.push(history.createLocation(location));
-    } else if ('object' === typeof location) {
+    } else if (typeof location === 'object') {
       // suport things like history.replace()
       const {type = 'push'} = (location as Location);
       if (type === 'go') {

history/src/serverHistory.ts
@@ -6,7 +6,7 @@ class ServerHistory implements History {
   private listeners: Array<Listener>;
   private _completeCallback: () => void;
 
-  constructor(private currentLocation: Location) {
+  constructor(private currentLocation: Location | null) {
     this.listeners = [];
   }
 
@@ -34,11 +34,11 @@ class ServerHistory implements History {
     return path;
   }
 
-  public createLocation(location: Location | Pathname) {
+  public createLocation(location: Location | Pathname): Location {
     return createLocation(location);
   }
 
-  public getCurrentLocation(): Location {
+  public getCurrentLocation(): Location | null {
     return this.currentLocation;
   }
 
@@ -51,6 +51,6 @@ class ServerHistory implements History {
   }
 }
 
-export function createServerHistory(loc: Location | Pathname | undefined): History {
-  return new ServerHistory(createLocation(loc));
+export function createServerHistory(loc?: Location | Pathname): History {
+  return new ServerHistory(loc ? createLocation(loc) : null);
 }

history/src/util.ts
@@ -28,14 +28,14 @@ const locationDefaults: Location = {
   action: 'POP',
   hash: '',
   search: '',
+  state: undefined,
+  key: null as any as string,
+  query: null as any as Object,
 };
 
-export function createLocation(location: Location | Pathname | undefined): Location {
+export function createLocation(location?: Location | Pathname): Location {
   if (typeof location === 'string') {
     return objectAssign({}, locationDefaults, {pathname: location});
-  } else if (typeof location === 'object') {
-    return objectAssign({}, locationDefaults, location);
-  } else {
-    return locationDefaults;
   }
+  return objectAssign({}, locationDefaults, location);
 }