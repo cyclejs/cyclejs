d86aaf22b59b980ef41178adfb1f8d7e110376a2
Andre Staltz <andre@staltz.com>
2016-10-27 12:12:18 +0200

    chore(META): fix devtool update-manifest script

--- devtool/.scripts/update-manifest.sh
+++ devtool/.scripts/update-manifest.sh
@@ -3,4 +3,4 @@
 JASE=$(dirname $0)/../../node_modules/.bin/jase
 TEMP=$(mktemp);
 cat $(dirname $0)/../package.json | $JASE version > ${TEMP};
-cat $(dirname $0)/../src/manifest.json | $JASE version --set $(cat $TEMP);
+cat $(dirname $0)/../src/manifest.json | $JASE version --set $(cat $TEMP) > $(dirname $0)/../src/manifest.json;
