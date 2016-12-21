d63312e810bc934b2fd65ebb89d0a255cc3983ac
dmitriz <dmitri14@gmail.com>
2016-12-21 14:47:21 +0000

    Annotate and format

--- examples/advanced/animated-letters/src/main.js
+++ examples/advanced/animated-letters/src/main.js
@@ -3,49 +3,84 @@ import xs from 'xstream'
 import pairwise from 'xstream/extra/pairwise'
 import fromEvent from 'xstream/extra/fromEvent'
 import {div, ul, li, makeDOMDriver} from '@cycle/dom'
+
 import {intersection, difference, sortBy} from 'lodash'
 
-function intent(keydownSource) {
-  return keydownSource
+// collect lodash utilities with familiar notation
+const _ = {
+  intersection,
+  difference,
+  sortBy
+}
+
+const intent = keydownSource =>
+  keydownSource
+
+    // -> stream of key names such as 'A'
     .map(ev => ev.code.replace('Key', ''))
     .filter(str => str.length === 1)
-}
 
-function model(action$) {
+
+const model = action$ => {
   const initialState = ['A']
-  return action$.startWith(initialState).fold((acc, key) => {
-    const index = acc.indexOf(key)
-    if (index === -1) {
-      return acc.concat(key).sort()
-    }
-    const newAcc = acc.slice()
-    newAcc.splice(index, 1)
-    return newAcc
-  }, [])
+
+  return action$
+    .startWith(initialState)
+    .fold((acc, key) => {
+      const index = acc.indexOf(key)
+      if (index === -1) {
+        return acc.concat(key).sort()
+      }
+      const newAcc = acc.slice()
+      newAcc.splice(index, 1)
+      return newAcc
+    }, [])
 }
 
-function determineDeltaPoints(state$) {
-  return state$.compose(pairwise).map(([before, after]) => {
-    const addedPoints = difference(after, before).map(key =>
-      ({key, value: 0, target: 1})
-    )
-    const removedPoints = difference(before, after).map(key =>
-      ({key, value: 1, target: 0})
-    )
-    const points = addedPoints.concat(removedPoints)
-    return xs.fromArray(sortBy(points, 'key'))
-  }).flatten()
-}
 
-function expandAsRenderingFrames(point$) {
-  return point$.map(point =>
-    xs.periodic(10).mapTo(point).take(100)
-  ).flatten()
-}
+const determineDeltaPoints = state$ => 
+  state$
 
-function calculateAnimationSteps(point$) {
+    // https://github.com/staltz/xstream/blob/master/src/extra/pairwise.ts#L50
+    // 
+    // Group consecutive pairs of events as arrays
+    // -> Stream<Array>
+    .compose(pairwise)
+    .map(([before, after]) => {
+
+      const addedPoints = _.difference(after, before)
+        .map(key =>
+          ({key, value: 0, target: 1})
+        )
+
+      const removedPoints = _.difference(before, after)
+        .map(key =>
+          ({key, value: 1, target: 0})
+        )
+
+      const points = addedPoints.concat(removedPoints)
+
+      return xs.fromArray(_.sortBy(points, 'key'))
+    })
+    .flatten()
+
+
+const expandAsRenderingFrames = point$ =>
+  point$.map(point =>
+    xs.periodic(10)
+      .mapTo(point)
+      .take(100)
+  )
+  .flatten()
+
+
+const calculateAnimationSteps = point$ => {
+
   function incorporateNewPoint(oldPoints, newPoint) {
-    const index = oldPoints.findIndex(point => point.key === newPoint.key)
+
+    const index = oldPoints
+      .findIndex(point => point.key === newPoint.key)
+
     let points
     if (index === -1 && newPoint.target === 1) {
       points = oldPoints.concat(newPoint)
@@ -60,12 +95,15 @@ function calculateAnimationSteps(point$) {
     return newPoints.map(newPoint => {
       const target = newPoint.target
       const oldPoint = oldPoints.find(p => p.key === newPoint.key)
-      const value = !!oldPoint ? oldPoint.value : newPoint.value
+      const value = !!oldPoint 
+        ? oldPoint.value 
+        : newPoint.value
+
       return {
         ...newPoint,
-        value: (Math.abs(target - value) < 0.01) ?
-          target :
-          value + (target - value) * 0.05
+        value: (Math.abs(target - value) < 0.01) 
+          ? target 
+          : value + (target - value) * 0.05
       }
     })
   }
@@ -76,39 +114,57 @@ function calculateAnimationSteps(point$) {
     const sanitizedAcc = progressedAcc.filter(point =>
       !(point.target === 0 && point.value === 0)
     )
-    const sortedAcc = sortBy(sanitizedAcc, 'key')
+    const sortedAcc = _.sortBy(sanitizedAcc, 'key')
     return sortedAcc
   }, [])
 }
 
-function animate(state$) {
-  return state$
+
+const animate = state$ =>
+  state$
     .compose(determineDeltaPoints)
     .compose(expandAsRenderingFrames)
     .compose(calculateAnimationSteps)
-}
+
 
 function view(state$) {
   const animatedState$ = animate(state$)
+
   const ulStyle = {padding: '0', listStyle: 'none', display: 'flex'}
   const liStyle = {fontSize: '50px'}
+
   return animatedState$.map(animStates =>
-    ul({style: ulStyle}, animStates.map(animState =>
-      li({style: {fontSize: `${animState.value * 50}px`}}, animState.key)
-    ))
-  )
-}
+
+    ul(
+      {style: ulStyle}, 
+      animStates.map(animState =>
+
+        li(
+          {style: {fontSize: `${animState.value * 50}px`}}, 
+          animState.key
+        )
+      
+      )
+    )
+
+  )}
 
 function main(sources) {
   const key$ = intent(sources.Keydown)
   const state$ = model(key$)
   const vtree$ = view(state$)
+
   return {
     DOM: vtree$,
   }
 }
 
+
 Cycle.run(main, {
+
+  // https://github.com/staltz/xstream/blob/master/src/extra/fromEvent.ts#L48
+  // 
+  // -> stream of Keydown events
   Keydown: () => fromEvent(document, 'keydown'),
   // DOM: makeDOMDriver('#main-container'),
   DOM: makeDOMDriver(document.body),
