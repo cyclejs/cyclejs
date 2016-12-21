bf256fba8b1e3f53a382d68d37cdb55f5d2bd0fe
dmitriz <dmitri14@gmail.com>
2016-12-21 02:58:26 +0000

    Reformat for better structure

--- examples/basic/counter/src/main.js
+++ examples/basic/counter/src/main.js
@@ -6,17 +6,27 @@ import {div, button, p, makeDOMDriver} from '@cycle/dom';
 
 function main(sources) {
   let action$ = xs.merge(
-    sources.DOM.select('.decrement').events('click').map(ev => -1),
-    sources.DOM.select('.increment').events('click').map(ev => +1)
+
+    sources.DOM.select('.decrement')
+      .events('click')
+      .map(ev => -1),
+
+    sources.DOM.select('.increment')
+      .events('click')
+      .map(ev => +1)
+
   );
   let count$ = action$.fold((x,y) => x + y, 0);
+
   return {
     DOM: count$.map(count =>
+
         div([
           button('.decrement', 'Decrement'),
           button('.increment', 'Increment'),
           p('Counter: ' + count)
         ])
+        
       )
   };
 }
