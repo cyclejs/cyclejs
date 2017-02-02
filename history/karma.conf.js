6311344d8adc5acedbb84562ce5035801c545384
Andre Staltz <andre@staltz.com>
2017-02-02 15:25:39 +0200

    test(history): improve Karma config with more SauceLabs browsers

--- history/karma.conf.js
+++ history/karma.conf.js
@@ -1,66 +1,120 @@
 module.exports = function (config) {
-  const configuration =
-    {
-      files: [
-        'src/**/*.ts',
-        'test/browser/**/*.ts',
+  const customLaunchers = {
+    SL_Chrome_49: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 10',
+      version: '49',
+    },
+
+    // TODO: fix these!
+    // SL_IE_10: {
+    //   base: 'SauceLabs',
+    //   browserName: 'internet explorer',
+    //   version: '10',
+    // },
+
+    // SL_IE_11: {
+    //   base: 'SauceLabs',
+    //   browserName: 'internet explorer',
+    //   version: '11',
+    // },
+
+    SL_Safari_8: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      version: '8',
+    },
+
+    SL_iphone_8: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      version: '8.4',
+    },
+
+    SL_iphone_9: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      version: '9.1',
+    },
+
+    SL_android_5_1: {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '5.1',
+    },
+
+    SL_Safari_Current: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      version: 'latest',
+    },
+
+    SL_MS_Edge: {
+      base: 'SauceLabs',
+      browserName: 'microsoftedge',
+    },
+
+    SL_Firefox_Current: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'Windows 10',
+      version: 'latest',
+    },
+
+    SL_Chrome_Current: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 10',
+      version: 'latest',
+    },
+  };
+
+  const configuration = {
+    sauceLabs: {
+      testName: 'Cycle History Karma tests on Sauce Labs',
+    },
+
+    files: [
+      'src/**/*.ts',
+      'test/browser/**/*.ts',
+    ],
+
+    frameworks: [
+      'mocha',
+      'karma-typescript'
+    ],
+
+    preprocessors: {
+      '**/*.ts': ['karma-typescript']
+    },
+
+    reporters: ['dots', 'saucelabs'],
+
+    concurrency: 1,
+
+    captureTimeout: 120000,
+
+    // LOG_DISABLE .. LOG_ERROR .. LOG_WARN .. LOG_INFO .. LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    customLaunchers: customLaunchers,
+
+    browsers: process.env.TRAVIS ? Object.keys(customLaunchers) : ['Chrome'],
+
+    karmaTypescriptConfig: {
+      tsconfig: 'tsconfig.json',
+      include: [
+        "test/browser/**/*.ts",
+        "src/**/*.ts"
       ],
-
-      frameworks: [
-        'mocha',
-        'karma-typescript'
-      ],
-
-      preprocessors: {
-        '**/*.ts': ['karma-typescript']
-      },
-
-      customLaunchers: {
-        Chrome_travis_ci: {
-          base: 'Chrome',
-          flags: ['--no-sandbox']
-        },
-
-        sl_ie_11: {
-          base: 'SauceLabs',
-          browserName: 'internet explorer',
-          platform: 'Windows 10',
-          version: '11'
-        },
-
-        sl_safari: {
-          base: 'SauceLabs',
-          browserName: 'safari',
-          version: '9'
-        },
-
-        // needs other configurations for SauceLabs
-      },
-
-      browsers: [
-        'Chrome',
-      ],
-
-      karmaTypescriptConfig: {
-        tsconfig: 'tsconfig.json',
-        include: [
-          "test/browser/**/*.ts",
-          "src/**/*.ts"
-        ],
-        reports: {
-          "html": "coverage",
-          "lcovonly": "coverage",
-        }
+      reports: {
+        "html": "coverage",
+        "lcovonly": "coverage",
       }
     }
-
-  if (process.env.TRAVIS) {
-    configuration.browsers = [
-      'Chrome_travis_ci',
-      'sl_ie_11',
-      'sl_safari'
-    ];
-  }
+  };
 
   config.set(configuration);
 }
