b3725fd13b3e0695d190d1c7f1831c3ca54d1d0c
Andre Staltz <andre@staltz.com>
2017-02-03 14:16:49 +0200

    refactor(examples): update hello-lastname for Cycle Unified

--- examples/hello-lastname/src/index.ts
+++ examples/hello-lastname/src/index.ts
@@ -1,7 +1,6 @@
 import xs, {Stream} from 'xstream';
-import Cycle from '@cycle/xstream-run';
-import {div, input, h2, button, p, makeDOMDriver, VNode} from '@cycle/dom';
-import {DOMSource} from '@cycle/dom/xstream-typings';
+import {run} from '@cycle/run';
+import {div, input, h2, button, p, makeDOMDriver, VNode, DOMSource} from '@cycle/dom';
 
 interface Sources {
   DOM: DOMSource;
@@ -33,10 +32,7 @@ function main(sources: Sources): Sinks {
     .map(([fn, ln]) => `${ln.toUpperCase()}, ${fn}`);
 
   const invalidName$ = rawFullName$
-    .filter(([fn, ln]) => {
-      console.log(fn.length, ln.length);
-      return fn.length === 0 || ln.length < 3
-    })
+    .filter(([fn, ln]) => fn.length === 0 || ln.length < 3)
     .mapTo('');
 
   const name$ = xs.merge(validName$, invalidName$);
@@ -45,14 +41,14 @@ function main(sources: Sources): Sinks {
     div([
       p([
         'First name',
-        input('.first', { attrs: { type: 'text' } })
+        input('.first', { attrs: { type: 'text' } }),
       ]),
       p([
         'Last name',
-        input('.last', { attrs: { type: 'text' } })
+        input('.last', { attrs: { type: 'text' } }),
       ]),
-      h2('Hello ' + name)
-    ])
+      h2('Hello ' + name),
+    ]),
   );
 
   return {
@@ -60,6 +56,6 @@ function main(sources: Sources): Sinks {
   };
 }
 
-Cycle.run(main, {
-  DOM: makeDOMDriver('#main-container')
+run(main, {
+  DOM: makeDOMDriver('#main-container'),
 });
