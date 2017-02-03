9dc5157e2ede825b03e957a7981a29cb88fd50b3
Andre Staltz <andre@staltz.com>
2017-02-03 14:07:28 +0200

    refactor(examples): update hello-world for Cycle Unified

--- examples/hello-world/src/main.js
+++ examples/hello-world/src/main.js
@@ -1,22 +1,24 @@
-import Cycle from '@cycle/xstream-run';
+import {run} from '@cycle/run';
 import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
 
 function main(sources) {
+  const vdom$ = sources.DOM
+    .select('.myinput').events('input')
+    .map(ev => ev.target.value)
+    .startWith('')
+    .map(name =>
+      div([
+        label('Name:'),
+        input('.myinput', {attrs: {type: 'text'}}),
+        hr(),
+        h1(`Hello ${name}`)
+      ])
+    );
   return {
-    DOM: sources.DOM.select('.myinput').events('input')
-      .map(ev => ev.target.value)
-      .startWith('')
-      .map(name =>
-        div([
-          label('Name:'),
-          input('.myinput', {attrs: {type: 'text'}}),
-          hr(),
-          h1(`Hello ${name}`)
-        ])
-      )
+    DOM: vdom$,
   };
 }
 
-Cycle.run(main, {
+run(main, {
   DOM: makeDOMDriver('#main-container')
 });
