7215ca9f6ec67d25eb70b3cb9d5c99afdde0108c
Andre Staltz <andre@staltz.com>
2017-02-11 16:18:35 +0200

    docs(META): make api reference HTML pages for each package
    Each package has the npm script docs, which will generate a Flatdoc HTML page in the /docs/api
    directory.

--- .scripts/make-api-docs.js
+++ .scripts/make-api-docs.js
@@ -1,24 +1,33 @@
 'use strict';
 var markdox = require('markdox');
-
-function generateDocs(options) {
-  markdox.process(
-    options.src,
-    {output: options.output, template: options.template},
-    function generationCallback(err/*, output */) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('File `' + options.output + '` generated with success');
-      }
-    }
-  );
-}
+var fs = require('fs');
 
 var argPackage = process.argv[2];
+var dirOfThePackage = __dirname + '/../' + argPackage;
 
-generateDocs({
-  src: __dirname + '/../' + argPackage + '/lib/index.js',
-  output: __dirname + '/../' + argPackage + '/generated-api.md',
-  template: __dirname + '/cycle-docs-template.md.ejs',
-});
+markdox.process(
+  dirOfThePackage + '/lib/index.js', // src
+  {
+    output: dirOfThePackage + '/generated-api.md',
+    template: __dirname + '/cycle-docs-template.md.ejs',
+  },
+  function generationCallback(err/*, output */) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('File `' + dirOfThePackage + '/generated-api.md' + '` generated with success');
+
+      const outputStr =
+        fs.readFileSync(__dirname + '/../docs/.scripts/api-ref-template-pre.html', 'utf-8') +
+        fs.readFileSync(dirOfThePackage + '/.scripts/docs-template.md', 'utf-8') +
+        fs.readFileSync(dirOfThePackage + '/generated-api.md', 'utf-8') +
+        fs.readFileSync(__dirname + '/../docs/.scripts/api-ref-template-post.html', 'utf-8');
+
+      const htmlFilename = __dirname + '/../docs/api/' + argPackage + '.html';
+      fs.writeFileSync(htmlFilename, outputStr, 'utf-8');
+      console.log('File `' + htmlFilename + '` generated with success');
+
+      fs.unlinkSync(dirOfThePackage + '/generated-api.md');
+    }
+  }
+);
