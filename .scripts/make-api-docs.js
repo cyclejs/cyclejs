5c2e6d411c4ff514c8b88749bfe14efed6d2ae60
Andre Staltz <andre@staltz.com>
2017-02-12 15:03:56 +0200

    docs(META): use ejs as an html template for API refs

--- .scripts/make-api-docs.js
+++ .scripts/make-api-docs.js
@@ -1,7 +1,11 @@
 'use strict';
 var markdox = require('markdox');
 var fs = require('fs');
+var ejs = require('ejs');
 
+var template = ejs.compile(
+  fs.readFileSync(__dirname + '/../docs/.scripts/template.html', 'utf-8')
+);
 var argPackage = process.argv[2];
 var dirOfThePackage = __dirname + '/../' + argPackage;
 
@@ -9,7 +13,7 @@ markdox.process(
   dirOfThePackage + '/lib/index.js', // src
   {
     output: dirOfThePackage + '/generated-api.md',
-    template: __dirname + '/cycle-docs-template.md.ejs',
+    template: __dirname + '/api-ref-template.md.ejs',
   },
   function generationCallback(err/*, output */) {
     if (err) {
@@ -17,13 +21,17 @@ markdox.process(
     } else {
       console.log('File `' + dirOfThePackage + '/generated-api.md' + '` generated with success');
 
-      const outputStr =
-        fs.readFileSync(__dirname + '/../docs/.scripts/api-ref-template-pre.html', 'utf-8') +
+      var content =
         fs.readFileSync(dirOfThePackage + '/.scripts/docs-template.md', 'utf-8') +
-        fs.readFileSync(dirOfThePackage + '/generated-api.md', 'utf-8') +
-        fs.readFileSync(__dirname + '/../docs/.scripts/api-ref-template-post.html', 'utf-8');
+        fs.readFileSync(dirOfThePackage + '/generated-api.md', 'utf-8');
 
-      const htmlFilename = __dirname + '/../docs/api/' + argPackage + '.html';
+      var outputStr = template({
+        title: 'API reference (' + argPackage + ')',
+        pathToRoot: '../',
+        content: content,
+      });
+
+      var htmlFilename = __dirname + '/../docs/api/' + argPackage + '.html';
       fs.writeFileSync(htmlFilename, outputStr, 'utf-8');
       console.log('File `' + htmlFilename + '` generated with success');
 
