16c23446f7d307fb021d3bde21a146e6a9b52962
Andre Staltz <andre@staltz.com>
2017-02-13 19:50:31 +0200

    docs(META): make api/index.html

---
+++ docs/.scripts/make-api-index.js
@@ -0,0 +1,26 @@
+'use strict';
+var fs = require('fs');
+var ejs = require('ejs');
+
+var template = ejs.compile(fs.readFileSync(__dirname + '/template.html', 'utf-8'));
+
+var mdFilename = __dirname + '/../content/api/index.md';
+var htmlFilename = __dirname + '/../api/index.html';
+var content = fs.readFileSync(mdFilename, 'utf-8');
+var outputStr = template({
+  title: 'API reference',
+  pathToRoot: '../',
+  content: content,
+  premenu: [],
+  postmenu: [
+    {title: 'Cycle Run', link: 'api/run.html'},
+    {title: 'Cycle RxJS Run', link: 'api/rxjs-run.html'},
+    {title: 'Cycle Most Run', link: 'api/most-run.html'},
+    {title: 'Cycle DOM', link: 'api/dom.html'},
+    {title: 'Cycle HTTP', link: 'api/http.html'},
+    {title: 'Cycle History', link: 'api/history.html'},
+    {title: 'Cycle Isolate', link: 'api/isolate.html'},
+    {title: 'Cycle JSONP', link: 'api/jsonp.html'},
+  ],
+});
+fs.writeFileSync(htmlFilename, outputStr, 'utf-8');
---
+++ docs/content/api/index.md
@@ -0,0 +1,10 @@
+# API reference
+
+- **[Cycle Run](run.html)**
+- **[Cycle RxJS Run](rxjs-run.html)**
+- **[Cycle Most Run](most-run.html)**
+- **[Cycle DOM](dom.html)**
+- **[Cycle HTTP](http.html)**
+- **[Cycle History](history.html)**
+- **[Cycle Isolate](isolate.html)**
+- **[Cycle JSONP](jsonp.html)**
