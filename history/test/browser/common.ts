cyclejs/cyclejs

commit d41bdae74904746e0a8983a77305f5bbf3a326c4

fix(history): rewrite for Cycle Unified
BREAKING CHANGES:
New API. You no longer need createHistory() calls that
create history objects to be passed to makeHistoryDriver(). You simply
call makeHistoryDriver() and those history objects are created under the
hood.

history/src/createHistory$.ts
@@ -1,27 +1,19 @@
-import { StreamAdapter } from '@cycle/base';
-import { Location, History, UnregisterCallback } from 'history';
-import { HistoryInput } from './types';
+import xs, {Stream, MemoryStream, Listener} from 'xstream';
+import {Location, History, UnregisterCallback} from 'history';
+import {HistoryInput} from './types';
 
-export function createHistory$ (history: History, sink$: any,
-                                runStreamAdapter: StreamAdapter): any {
-  const push = makePushState(history);
-
-  const { observer, stream } = runStreamAdapter.makeSubject<Location>();
-
-  const history$ = runStreamAdapter.remember<Location>(stream);
-
-  const unlisten = history.listen((loc: Location) => {
-    observer.next(loc);
-  });
-
-  (history$ as any).dispose =
-    runStreamAdapter.streamSubscribe(sink$, createObserver(push, unlisten));
-
+export function createHistory$(history: History,
+                               sink$: Stream<HistoryInput | string>): MemoryStream<Location> {
+  const history$ = xs.createWithMemory<Location>();
+  const call = makeCallOnHistory(history);
+  const unlisten = history.listen((loc: Location) => { history$._n(loc); });
+  const sub = sink$.subscribe(createObserver(call, unlisten));
+  (history$ as any).dispose = () => { sub.unsubscribe(); unlisten(); };
   return history$;
 };
 
-function makePushState (history: History) {
-  return function pushState (input: HistoryInput): void {
+function makeCallOnHistory(history: History) {
+  return function call(input: HistoryInput): void {
     if (input.type === 'push') {
       history.push(input.pathname, input.state);
     }
@@ -44,17 +36,17 @@ function makePushState (history: History) {
   };
 }
 
-function createObserver (push: (input: HistoryInput) => any,
-                         unlisten: UnregisterCallback) {
+function createObserver(call: (input: HistoryInput) => void,
+                        unlisten: UnregisterCallback): Listener<HistoryInput | string> {
   return {
-    next (input: HistoryInput | String) {
+    next (input: HistoryInput | string) {
       if (typeof input === 'string') {
-        push({ type: 'push', pathname: input });
+        call({type: 'push', pathname: input});
       } else {
-        push(input as HistoryInput);
+        call(input);
       }
     },
-    error: unlisten,
-    complete: unlisten,
+    error: (err) => { unlisten(); },
+    complete: () => { setTimeout(unlisten); },
   };
 }

history/test/browser/common.ts
@@ -1,0 +1,24 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+import * as assert from 'assert';
+import {makeHashHistoryDriver, makeHistoryDriver} from '../../src';
+
+describe('makeHistoryDriver', () => {
+  it('should be a function', () => {
+    assert.strictEqual(typeof makeHistoryDriver, 'function');
+  });
+
+  it('should return a function' , () => {
+    assert.strictEqual(typeof makeHistoryDriver(), 'function');
+  });
+});
+
+describe('makeHashHistoryDriver', () => {
+  it('should be a function', () => {
+    assert.strictEqual(typeof makeHashHistoryDriver, 'function');
+  });
+
+  it('should return a function' , () => {
+    assert.strictEqual(typeof makeHashHistoryDriver(), 'function');
+  });
+});

history/test/browser/rxjs.ts
@@ -1,0 +1,124 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+import * as assert from 'assert';
+import {Observable} from 'rxjs';
+import {setup, run} from '@cycle/rxjs-run';
+import {makeHashHistoryDriver, captureClicks, Location, HistoryInput} from '../../src';
+
+let dispose = () => {};
+
+describe('historyDriver - RxJS', () => {
+  beforeEach(function () {
+    dispose();
+  });
+
+  it('should return a stream', () => {
+    function main(sources: {history: Observable<Location>}) {
+      assert.strictEqual(typeof sources.history.switchMap, 'function');
+      return {
+        history: Observable.never(),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+    assert.strictEqual(typeof sources.history.switchMap, 'function');
+  });
+
+  it('should create a location from pathname', function (done) {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of('/test'),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should create a location from PushHistoryInput', function (done) {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of({type: 'push', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should create a location from ReplaceHistoryInput', function (done) {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of({type: 'replace', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should allow going back/forwards with `go`, `goBack`, `goForward`', function (done) {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.interval(100).take(6).map(i => [
+          '/test',
+          '/other',
+          {type: 'go', amount: -1},
+          {type: 'go', amount: +1},
+          {type: 'goBack'},
+          {type: 'goForward'},
+        ][i]),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+});

history/test/browser/xstream.ts
@@ -1,0 +1,126 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+import * as assert from 'assert';
+import xs, {Stream} from 'xstream';
+import {setup, run} from '@cycle/run';
+import {setAdapt} from '@cycle/run/lib/adapt';
+import {makeHashHistoryDriver, captureClicks, Location, HistoryInput} from '../../src';
+
+let dispose = () => {};
+
+describe('historyDriver - xstream', () => {
+  beforeEach(function () {
+    setAdapt(x => x);
+    dispose();
+  });
+
+  it('should return a stream', () => {
+    function main(sources: {history: Stream<Location>}) {
+      assert.strictEqual(typeof sources.history.remember, 'function');
+      return {
+        history: xs.never(),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+    assert.strictEqual(typeof sources.history.remember, 'function');
+  });
+
+  it('should create a location from pathname', function (done) {
+    function main(sources: {history: Stream<Location>}) {
+      return {
+        history: xs.of('/test'),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should create a location from PushHistoryInput', function (done) {
+    function main(sources: {history: Stream<Location>}) {
+      return {
+        history: xs.of({type: 'push', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should create a location from ReplaceHistoryInput', function (done) {
+    function main(sources: {history: Stream<Location>}) {
+      return {
+        history: xs.of({type: 'replace', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+
+  it('should allow going back/forwards with `go`, `goBack`, `goForward`', function (done) {
+    function main(sources: {history: Stream<Location>}) {
+      return {
+        history: xs.periodic(100).take(6).map(i => [
+          '/test',
+          '/other',
+          {type: 'go', amount: -1},
+          {type: 'go', amount: +1},
+          {type: 'goBack'},
+          {type: 'goForward'},
+        ][i]),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeHashHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    dispose = run();
+  });
+});

history/test/node/common.ts
@@ -1,0 +1,14 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+import * as assert from 'assert';
+import {makeServerHistoryDriver} from '../../src';
+
+describe('makeServerHistoryDriver', () => {
+  it('should be a function', () => {
+    assert.strictEqual(typeof makeServerHistoryDriver, 'function');
+  });
+
+  it('should return a function' , () => {
+    assert.strictEqual(typeof makeServerHistoryDriver(), 'function');
+  });
+});

history/test/node/rxjs.ts
@@ -1,0 +1,212 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+import * as assert from 'assert';
+import {Observable} from 'rxjs';
+import {setup, run} from '@cycle/rxjs-run';
+import {makeServerHistoryDriver, Location, HistoryInput} from '../../src';
+
+describe('serverHistoryDriver - RxJS', function () {
+  it('should return an Rx Observable as source', function () {
+    function main(sources: {history: Observable<Location>}) {
+      assert.strictEqual(typeof sources.history.switchMap, 'function');
+      return {
+        history: Observable.never(),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+    assert.strictEqual(typeof sources.history.switchMap, 'function');
+  });
+
+  it('should create a location from pathname', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of('/test'),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should create a location from PushHistoryInput', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of({type: 'push', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should create a location from ReplaceHistoryInput', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of({type: 'replace', pathname: '/test'}),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, '/test');
+        done();
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should allow going back a route with type `go`', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of<HistoryInput | string>(
+          '/test',
+          '/other',
+          { type: 'go', amount: -1 },
+        ),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should allow going back a route with type `goBack`', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of<HistoryInput | string>(
+          '/test',
+          '/other',
+          {type: 'goBack'},
+        ),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should allow going forward a route with type `go`', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of<HistoryInput | string>(
+          '/test',
+          '/other',
+          {type: 'go', amount: -1},
+          {type: 'go', amount: 1},
+        ),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+
+  it('should allow going forward a route with type `goForward`', (done) => {
+    function main(sources: {history: Observable<Location>}) {
+      return {
+        history: Observable.of<HistoryInput | string>(
+          '/test',
+          '/other',
+          {type: 'go', amount: -1},
+          {type: 'goForward'},
+        ),
+      };
+    }
+
+    const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
+
+    const expected = [
+      '/test',
+      '/other',
+      '/test',
+      '/other',
+    ];
+
+    sources.history.subscribe({
+      next (location: Location) {
+        assert.strictEqual(location.pathname, expected.shift());
+        if (expected.length === 0) {
+          done();
+        }
+      },
+      error: done,
+      complete: () => { done('complete should not be called'); },
+    });
+    run();
+  });
+});

commit 973ee9a2fb97ed1e2c30489f5e44985ff8a15927

fix(history): fix history drivers to start emitting the current location
This fixes a bug introduced in d41bdae74904746e0a8983a77305f5bbf3a326c4

history/src/createHistory$.ts
@@ -4,7 +4,7 @@ import {HistoryInput} from './types';
 
 export function createHistory$(history: History,
                                sink$: Stream<HistoryInput | string>): MemoryStream<Location> {
-  const history$ = xs.createWithMemory<Location>();
+  const history$ = xs.createWithMemory<Location>().startWith(history.location);
   const call = makeCallOnHistory(history);
   const unlisten = history.listen((loc: Location) => { history$._n(loc); });
   const sub = sink$.subscribe(createObserver(call, unlisten));

history/test/browser/common.ts
@@ -14,6 +14,23 @@ describe('makeHistoryDriver', () => {
   it('should return a function' , () => {
     assert.strictEqual(typeof makeHistoryDriver(), 'function');
   });
+
+  it('should start emitting the current location', function (done) {
+    const history$ = makeHistoryDriver()(xs.never());
+
+    const sub = history$.subscribe({
+      next: (location: Location) => {
+        assert(location.pathname);
+        done();
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    });
+  });
 });
 
 describe('makeHashHistoryDriver', () => {
@@ -24,6 +41,23 @@ describe('makeHashHistoryDriver', () => {
   it('should return a function' , () => {
     assert.strictEqual(typeof makeHashHistoryDriver(), 'function');
   });
+
+  it('should start emitting the current location', function (done) {
+    const history$ = makeHashHistoryDriver()(xs.never());
+
+    const sub = history$.subscribe({
+      next: (location: Location) => {
+        assert(location.pathname);
+        done();
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    });
+  });
 });
 
 describe('captureClicks', () => {
@@ -31,7 +65,7 @@ describe('captureClicks', () => {
     const historyDriver = makeHistoryDriver();
     const history$ = captureClicks(historyDriver)(xs.never());
 
-    const sub = history$.subscribe({
+    const sub = history$.drop(1).subscribe({
       next: (location: Location) => {
         assert.strictEqual(location.pathname, '/test');
         sub.unsubscribe();

history/test/browser/rxjs.ts
@@ -38,7 +38,7 @@ describe.skip('historyDriver - RxJS', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -58,7 +58,7 @@ describe.skip('historyDriver - RxJS', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -78,7 +78,7 @@ describe.skip('historyDriver - RxJS', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -114,7 +114,7 @@ describe.skip('historyDriver - RxJS', () => {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {

history/test/browser/xstream.ts
@@ -37,7 +37,7 @@ describe('historyDriver - xstream', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -57,7 +57,7 @@ describe('historyDriver - xstream', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -77,7 +77,7 @@ describe('historyDriver - xstream', () => {
 
     const {sources, run} = setup(main, { history: makeHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -113,7 +113,7 @@ describe('historyDriver - xstream', () => {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {

history/test/node/common.ts
@@ -1,7 +1,8 @@
 /// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 /// <reference path="../../node_modules/@types/node/index.d.ts" />
 import * as assert from 'assert';
-import {makeServerHistoryDriver} from '../../src';
+import {Location, makeServerHistoryDriver} from '../../src';
+import xs from 'xstream';
 
 describe('makeServerHistoryDriver', () => {
   it('should be a function', () => {
@@ -10,5 +11,22 @@ describe('makeServerHistoryDriver', () => {
 
   it('should return a function' , () => {
     assert.strictEqual(typeof makeServerHistoryDriver(), 'function');
+  });
+
+  it('should start emitting the current location', function (done) {
+    const history$ = makeServerHistoryDriver()(xs.never());
+
+    const sub = history$.subscribe({
+      next: (location: Location) => {
+        assert(location.pathname);
+        done();
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    });
   });
 });

history/test/node/rxjs.ts
@@ -27,7 +27,7 @@ describe('serverHistoryDriver - RxJS', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -47,7 +47,7 @@ describe('serverHistoryDriver - RxJS', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -67,7 +67,7 @@ describe('serverHistoryDriver - RxJS', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -97,7 +97,7 @@ describe('serverHistoryDriver - RxJS', function () {
       '/test',
     ];
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -129,7 +129,7 @@ describe('serverHistoryDriver - RxJS', function () {
       '/test',
     ];
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -163,7 +163,7 @@ describe('serverHistoryDriver - RxJS', function () {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -197,7 +197,7 @@ describe('serverHistoryDriver - RxJS', function () {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.skip(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {

history/test/node/xstream.ts
@@ -32,7 +32,7 @@ describe('serverHistoryDriver - xstream', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -52,7 +52,7 @@ describe('serverHistoryDriver - xstream', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -72,7 +72,7 @@ describe('serverHistoryDriver - xstream', function () {
 
     const {sources, run} = setup(main, { history: makeServerHistoryDriver() });
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, '/test');
         done();
@@ -102,7 +102,7 @@ describe('serverHistoryDriver - xstream', function () {
       '/test',
     ];
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -134,7 +134,7 @@ describe('serverHistoryDriver - xstream', function () {
       '/test',
     ];
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -168,7 +168,7 @@ describe('serverHistoryDriver - xstream', function () {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {
@@ -202,7 +202,7 @@ describe('serverHistoryDriver - xstream', function () {
       '/other',
     ];
 
-    sources.history.subscribe({
+    sources.history.drop(1).subscribe({
       next (location: Location) {
         assert.strictEqual(location.pathname, expected.shift());
         if (expected.length === 0) {