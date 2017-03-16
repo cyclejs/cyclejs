47e84cf51d7e5f3cc36d2588a32e6152017d4f73
vvo <vincent.voyer@algolia.com>
2017-03-16 12:25:20 +0100

    docs(search): Add Algolia's DocSearch
    Allows searching inside the documentation

---
+++ docs/docsearch.js
@@ -0,0 +1,7 @@
+docsearch({
+  apiKey: '9294b142b28d26be38f28f29a5d69b38',
+  indexName: 'cycle_js',
+  inputSelector: '#docsearch',
+  enhancedSearchInput: true,
+  debug: false // Set debug to true if you want to inspect the dropdown
+});
