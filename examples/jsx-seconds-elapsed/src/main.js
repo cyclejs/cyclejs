a1e11dae0ff819559c73cc46220a751fa958aaa8
Nick Johnstone <ncwjohnstone@gmail.com>
2017-04-14 14:34:24 +1200

    chore(examples): update jsx-seconds-elapsed to use @cycle/time

--- examples/jsx-seconds-elapsed/src/main.js
+++ examples/jsx-seconds-elapsed/src/main.js
@@ -1,10 +1,11 @@
 import xs from 'xstream';
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
+import {timeDriver} from '@cycle/time';
 import {html} from 'snabbdom-jsx';
 
 function main(sources) {
-  const vdom$ = xs.periodic(1000).map(i => i + 1).startWith(0)
+  const vdom$ = sources.Time.periodic(1000).map(i => i + 1).startWith(0)
     .map(i =>
       <div>Seconds elapsed {i}</div>
     );
@@ -16,4 +17,5 @@ function main(sources) {
 
 run(main, {
   DOM: makeDOMDriver('#main-container'),
+  Time: timeDriver
 });
