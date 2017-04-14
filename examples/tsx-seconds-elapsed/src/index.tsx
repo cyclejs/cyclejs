97ff9341aedf74e6a7c87333be79b2a9b5fe36f6
Nick Johnstone <ncwjohnstone@gmail.com>
2017-04-14 14:45:20 +1200

    chore(examples): update tsx-seconds-elapsed to use @cycle/time

--- examples/tsx-seconds-elapsed/src/index.tsx
+++ examples/tsx-seconds-elapsed/src/index.tsx
@@ -1,10 +1,12 @@
 import xs, {Stream} from 'xstream';
 import {run} from '@cycle/run';
 import {makeDOMDriver, DOMSource, VNode} from '@cycle/dom';
+import {timeDriver, TimeSource} from '@cycle/time';
 const {html} = require('snabbdom-jsx');
 
 type Sources = {
   DOM: DOMSource;
+  Time: TimeSource;
 };
 
 type Sinks = {
@@ -12,8 +14,8 @@ type Sinks = {
 };
 
 function main(sources: Sources): Sinks {
-   const vdom$ = xs.periodic(1000).map(i => i + 1).startWith(0)
-     .map(i => <div>Seconds elapsed {i}</div>);
+  const vdom$ = sources.Time.periodic(1000).map(i => i + 1).startWith(0)
+    .map(i => <div>Seconds elapsed {i}</div>);
 
   return {
     DOM: vdom$,
@@ -22,4 +24,5 @@ function main(sources: Sources): Sinks {
 
 run(main, {
   DOM: makeDOMDriver('#main-container'),
+  Time: timeDriver
 });
