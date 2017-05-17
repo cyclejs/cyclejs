3a568834e4c8ee2904a0fc5f6453f647951ae189
Guy Rozen <holyeggplant@gmail.com>
2017-05-18 00:19:47 +0300

    refactor(dom): preventDefault does not manipulate stream

--- dom/src/fromEvent.ts
+++ dom/src/fromEvent.ts
@@ -2,12 +2,21 @@ import {Stream, Producer, Listener} from 'xstream';
 
 export function fromEvent(element: Element | Document,
                           eventName: string,
-                          useCapture = false): Stream<Event> {
+                          useCapture = false,
+                          preventDefault = false): Stream<Event> {
   return Stream.create<Event>({
     element: element,
     next: null,
     start: function start(listener: Listener<Event>) {
-      this.next = function next(event: Event) { listener.next(event); };
+      if (preventDefault) {
+        this.next = function next(event: Event) {
+          event.preventDefault();
+          listener.next(event);
+        };
+      } else {
+        this.next = function next(event: Event) { listener.next(event); };
+      }
+
       this.element.addEventListener(eventName, this.next, useCapture);
     },
     stop: function stop() {
