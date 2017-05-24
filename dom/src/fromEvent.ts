f2ba007fe84f39d1bde2a27c73b14a15e17ccec3
Jan van Brügge <supermanitu@gmail.com>
2017-05-24 16:48:13 +0200

    feat(dom): allow predicate function and object
    ISSUES CLOSED: #611

--- dom/src/fromEvent.ts
+++ dom/src/fromEvent.ts
@@ -1,10 +1,14 @@
 import {Stream, Producer, Listener} from 'xstream';
 
+export type Predicate = (ev : any) => boolean;
+export type PreventDefaultOpt = boolean | Predicate | Comparator;
+export type Comparator = { [key: string]: any };
+
 export function fromEvent(
   element: Element | Document,
   eventName: string,
   useCapture = false,
-  preventDefault = false,
+  preventDefault: PreventDefaultOpt = false,
 ): Stream<Event> {
   return Stream.create<Event>(
     {
@@ -13,7 +17,7 @@ export function fromEvent(
       start: function start(listener: Listener<Event>) {
         if (preventDefault) {
           this.next = function next(event: Event) {
-            event.preventDefault();
+            preventDefaultConditional(event, preventDefault);
             listener.next(event);
           };
         } else {
@@ -21,7 +25,6 @@ export function fromEvent(
             listener.next(event);
           };
         }
-
         this.element.addEventListener(eventName, this.next, useCapture);
       },
       stop: function stop() {
@@ -29,4 +32,26 @@ export function fromEvent(
       },
     } as Producer<Event>,
   );
+}
+
+export function preventDefaultConditional(event: any, preventDefault: PreventDefaultOpt) : void {
+  if (preventDefault) {
+    if (typeof preventDefault === 'boolean') {
+      event.preventDefault();
+    } else if (typeof preventDefault === 'function') {
+      if (preventDefault(event)) {
+        event.preventDefault();
+      }
+    } else if (typeof preventDefault === 'object') {
+      const matches = Object.keys(preventDefault)
+        .map(k => [k, preventDefault[k]])
+        .reduce((acc, [k, v]) => acc && event[k] === v, true);
+
+      if (matches) {
+        event.preventDefault();
+      }
+    } else {
+      throw new Error('preventDefault has to be either a boolean, predicate function or object');
+    }
+  }
 }
