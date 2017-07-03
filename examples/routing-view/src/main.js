69779ba8962a3c3dfdb13a1dc8e68718de4c8741
Churchill Lee <churchill@piktochart.com>
2017-07-04 00:16:05 +0800

    docs(example): add comments in the routing-view example

--- examples/routing-view/src/main.js
+++ examples/routing-view/src/main.js
@@ -5,18 +5,26 @@ import {makeHashHistoryDriver} from '@cycle/history';
 import {h, makeDOMDriver} from '@cycle/dom';
 
 function main(sources) {
-  const history$ = sources.DOM.select('h4').events('click')
-    .map(e => e.target.dataset.url)
+  // stream of strings to be passed into the history driver
+  const history$ = sources.DOM.select('nav').events('click')
+    .map(e => e.target.dataset.page)
     .compose(dropRepeats())
 
   return {
+    // ideally you will map this into your state or something first before
+    // displaying it as a view.
+    // here you can get creative and do whatever you want.
+    // even an if-else statement to map to different views entirely
     DOM: sources.history.map(view),
+
+    // history driver accepts either objects or string to update the url
     history: history$
   };
 }
 
 function view({ pathname, search, hash }) {
   return h('div', [
+    navigation( pathname ),
     h('h1', 'History object'),
     h('h3', [
       h('span', 'Pathname: '),
@@ -29,12 +37,25 @@ function view({ pathname, search, hash }) {
     h('h3', [
       h('span', 'Hash: '),
       h('label', hash )
-    ]),
-    h('h1', 'Links'),
-    h('h4', { dataset: { url: 'cat' }}, 'Cat'),
-    h('h4', { dataset: { url: 'dog' }}, 'Dog'),
-    h('h4', { dataset: { url: 'mouse' }}, 'Mouse'),
+    ])
   ]);
+}
+
+function navigation( pathname ) {
+  return h('nav', {}, [
+    h('span', {
+      dataset : { page: 'home' },
+      class : { 'active': pathname === '/home' }
+    }, 'Home'),
+    h('span', {
+      dataset : { page: 'about' },
+      class : { 'active': pathname === '/about' }
+    }, 'About'),
+    h('span', {
+      dataset : { page: 'contacts' },
+      class : { 'active': pathname === '/contacts' }
+    }, 'contacts')
+  ])
 }
 
 run(main, {
