dbb79996a9d5e562cb97d2e221b006f84adb2a12
Churchill Lee <churchill@piktochart.com>
2017-07-04 00:37:03 +0800

    feat(examples): add placeholder text and split page views

--- examples/routing-view/src/main.js
+++ examples/routing-view/src/main.js
@@ -4,6 +4,8 @@ import {run} from '@cycle/run';
 import {makeHashHistoryDriver} from '@cycle/history';
 import {h, makeDOMDriver} from '@cycle/dom';
 
+import placeholderText from 'lorem-ipsum';
+
 function main(sources) {
   // stream of strings to be passed into the history driver
   const history$ = sources.DOM.select('nav').events('click')
@@ -14,7 +16,8 @@ function main(sources) {
     // ideally you will map this into your state or something first before
     // displaying it as a view.
     // here you can get creative and do whatever you want.
-    // even an if-else statement to map to different views entirely
+    // an if-else statement to map to different views entirely
+    // or maps to an entire component call directly.
     DOM: sources.history.map(view),
 
     // history driver accepts either objects or string to update the url
@@ -22,22 +25,35 @@ function main(sources) {
   };
 }
 
-function view({ pathname, search, hash }) {
+function view({ pathname, search, hash, state }) {
+  const page = pathname === '/about' ?
+                aboutPageView() :
+                pathname === '/contacts' ?
+                  contactsPageView() :
+                  homePageView();
+
   return h('div', [
     navigation( pathname ),
-    h('h1', 'History object'),
-    h('h3', [
+    page,
+    h('br'),
+    h('h3', 'History object'),
+    h('h4', [
       h('span', 'Pathname: '),
       h('label', pathname )
     ]),
-    h('h3', [
+    h('h4', [
       h('span', 'Search: '),
       h('label', search )
     ]),
-    h('h3', [
+    h('h4', [
       h('span', 'Hash: '),
       h('label', hash )
-    ])
+    ]),
+    h('h4', [
+      h('span', 'State: '),
+      h('label', JSON.stringify(state) )
+    ]),
+
   ]);
 }
 
@@ -55,6 +71,27 @@ function navigation( pathname ) {
       dataset : { page: 'contacts' },
       class : { 'active': pathname === '/contacts' }
     }, 'Contacts')
+  ])
+}
+
+function homePageView() {
+  return h('div', {}, [
+    h('h1', 'Welcome to History Examples!'),
+    h('p', placeholderText())
+  ])
+}
+
+function aboutPageView() {
+  return h('div', {}, [
+    h('h1', 'About me'),
+    h('p', placeholderText())
+  ])
+}
+
+function contactsPageView() {
+  return h('div', {}, [
+    h('h1', 'Contact me'),
+    h('p', placeholderText())
   ])
 }
 
