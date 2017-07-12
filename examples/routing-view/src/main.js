c0d2e0f5a64e7c35a73c0d83a7a82db25a42189c
Churchill Lee <FeliciousX@users.noreply.github.com>
2017-07-13 02:19:41 +0800

    docs(examples): add a simple history/routing example
    PR #643
    * feat(examples): add initial commit for routing-view example
    
    * chore(package.json): remove watchify and the likes
    
    * docs(example): add comments in the routing-view example
    
    * feat(examples): add some styling
    
    * feat(examples): add placeholder text and split page views
    
    * feat(examples): add 404 page
    
    * docs(examples): fix typo in comments for routing-view
    
    * feat(examples): improve css styling
    
    * feat(example): show json stringified history object
    
    - also added debug so people can see the console output
    
    * chore(package-lock): update package-lock.json

---
+++ examples/routing-view/src/main.js
@@ -0,0 +1,91 @@
+import xs from 'xstream';
+import dropRepeats from 'xstream/extra/dropRepeats';
+import {run} from '@cycle/run';
+import {makeHashHistoryDriver} from '@cycle/history';
+import {h, makeDOMDriver} from '@cycle/dom';
+
+import placeholderText from 'lorem-ipsum';
+
+function main(sources) {
+  // stream of strings to be passed into the history driver
+  const history$ = sources.DOM.select('nav').events('click')
+    .map(e => e.target.dataset.page)
+    .compose(dropRepeats())
+
+  return {
+    // sources.history emits a history object each time there's a route change
+    // ideally you will map this into your state or something first before
+    // displaying it as a view. then you can do whatever you want...
+    // an if-else statement to map to different views entirely
+    // or maps to an entire component call directly
+    // or use your own pattern matching library. etc
+    DOM: sources.history.debug('history').map(view),
+
+    // history driver accepts either objects or string to update the url
+    history: history$
+  };
+}
+
+function view(history) {
+  const { pathname } = history;
+  let page = h('h1', '404 not found');
+  if (pathname === '/home') {
+    page = homePageView();
+  } else if (pathname === '/about') {
+    page = aboutPageView();
+  } else if (pathname === '/contacts') {
+    page = contactsPageView();
+  }
+
+  return h('div', [
+    navigation( pathname ),
+    page,
+    h('br'),
+    h('h3', 'History object'),
+    h('p', JSON.stringify(history))
+  ]);
+}
+
+function navigation( pathname ) {
+  return h('nav', {}, [
+    h('span', {
+      dataset : { page: 'home' },
+      class : { 'active': pathname === '/home' }
+    }, 'Home'),
+    h('span', {
+      dataset : { page: 'about' },
+      class : { 'active': pathname === '/about' }
+    }, 'About'),
+    h('span', {
+      dataset : { page: 'contacts' },
+      class : { 'active': pathname === '/contacts' }
+    }, 'Contacts')
+  ])
+}
+
+function homePageView() {
+  return h('div', {}, [
+    h('h1', 'Welcome to History Examples!'),
+    h('p', placeholderText())
+  ])
+}
+
+function aboutPageView() {
+  return h('div', {}, [
+    h('h1', 'About me'),
+    h('p', placeholderText())
+  ])
+}
+
+function contactsPageView() {
+  return h('div', {}, [
+    h('h1', 'Contact me'),
+    h('p', placeholderText())
+  ])
+}
+
+run(main, {
+  DOM: makeDOMDriver('#app'),
+  history: makeHashHistoryDriver()
+});
+
