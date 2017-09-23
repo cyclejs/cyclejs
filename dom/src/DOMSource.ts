b9db15c4abb277ba95cc314dd99d18c1ee553e99
Rick Koster <ngf.koster@outlook.com>
2017-09-23 16:42:53 +0200

    feat(dom): overload .event() method using HTMLElementEventMap (#682)
    The lib.dom.d.ts file contains a mapping where the name of the event type is mapped to the event
    type itself (e.g. click -> MouseEvent). Using this mapping on the DOMSource.events() function allows
    the correct event stream to be returned.

--- dom/src/DOMSource.ts
+++ dom/src/DOMSource.ts
@@ -8,5 +8,9 @@ export interface EventsFnOptions {
 export interface DOMSource {
   select(selector: string): DOMSource;
   elements(): MemoryStream<Document | Element | Array<Element> | string>;
+  events<K extends keyof HTMLElementEventMap>(
+    eventType: K,
+    options?: EventsFnOptions,
+  ): Stream<HTMLElementEventMap[K]>;
   events(eventType: string, options?: EventsFnOptions): Stream<Event>;
 }
