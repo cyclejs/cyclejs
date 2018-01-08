3cd783207aa0b0ac6f351712b3f6a8bf8be8f3c7
Nick Johnstone <ncwjohnstone@gmail.com>
2018-01-09 11:12:18 +1300

    test(time): add a test for real time driver propagating events

--- time/test/time-driver.ts
+++ time/test/time-driver.ts
@@ -1,4 +1,3 @@
-
 import * as assert from 'assert';
 import {timeDriver, TimeSource, Operator} from '../';
 import xs, {Stream} from 'xstream';
@@ -7,14 +6,42 @@ import {setAdapt} from '@cycle/run/lib/adapt';
 describe('time driver', () => {
   before(() => setAdapt(s => s));
 
-  it('propagates errors', (done) => {
+  it('propagates events passed through operators', done => {
     const Time = timeDriver(xs.empty());
 
-    xs.throw(new Error()).compose(Time.debounce(1)).addListener({
-      error (err: Error) {
-        done();
-        Time.dispose();
-      }
-    })
+    const expected = [1, 2, 3];
+
+    xs
+      .of(1, 2, 3)
+      .compose(Time.delay(1))
+      .addListener({
+        next(n: number) {
+          assert.equal(n, expected.shift());
+        },
+
+        complete() {
+          assert.equal(expected.length, 0);
+
+          done();
+
+          Time.dispose();
+        },
+
+        error: done,
+      });
+  });
+
+  it('propagates errors', done => {
+    const Time = timeDriver(xs.empty());
+
+    xs
+      .throw(new Error())
+      .compose(Time.debounce(1))
+      .addListener({
+        error(err: Error) {
+          done();
+          Time.dispose();
+        },
+      });
   });
 });
