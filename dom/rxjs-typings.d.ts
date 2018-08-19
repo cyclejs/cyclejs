c19ffeac6b13aec4792ca089187018bac2335b49
Jan van Brügge <supermanitu@gmail.com>
2018-08-19 11:21:14 +0300

    fix(dom): fix rxjs and most typings
    ISSUES CLOSED: #775

--- dom/rxjs-typings.d.ts
+++ dom/rxjs-typings.d.ts
@@ -1,7 +1,12 @@
-import {EventsFnOptions} from './lib/DOMSource';
+import {EventsFnOptions} from './lib/es6/DOMSource';
 import {Observable} from 'rxjs';
 export interface DOMSource {
   select(selector: string): DOMSource;
   elements(): Observable<Element | Array<Element>>;
+  events<K extends keyof HTMLElementEventMap>(
+    eventType: K,
+    options?: EventsFnOptions,
+    bubbles?: boolean
+  ): Observable<HTMLElementEventMap[K]>;
   events(eventType: string, options?: EventsFnOptions): Observable<Event>;
 }
