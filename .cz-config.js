3249855fecba3280b568f37093cc3c1a1f3abe11
Andre Staltz <andre@staltz.com>
2018-08-23 17:36:58 +0300

    chore(META): remove some commit types, use chore type more

--- .cz-config.js
+++ .cz-config.js
@@ -19,20 +19,14 @@ module.exports = {
   types: [
     {value: 'feat', name: 'feat:     Add a new feature'},
     {value: 'fix', name: 'fix:      Submit a bug fix'},
-    {
-      value: 'refactor',
-      name:
-        'refactor: A code change that neither fixes a bug nor adds a feature.\n            Includes code style changes.',
-    },
-    {value: 'test', name: 'test:     Add tests only'},
     {value: 'docs', name: 'docs:     Documentation only changes'},
     {value: 'release', name: 'release:  Publish a new version of a package.'},
     {
       value: 'chore',
       name:
-        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation. META only.',
+        'chore:    Any internal changes that do not affect the\n            ' +
+        'the users of packages. Includes refactoring.',
     },
-    {value: 'perf', name: 'perf:     A code change that improves performance'},
   ],
 
   scopes: packageScopes
@@ -41,7 +35,7 @@ module.exports = {
     .map(name => ({name})),
 
   scopeOverrides: {
-    chore: [{name: 'META'}],
+    chore: packageScopes.concat(otherScopes),
     feat: packageScopes,
     fix: packageScopes,
     release: packageScopes,
