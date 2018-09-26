8ed89e85f9a9f980d77cc9673548e2e39a9b3b9b
Andre Staltz <andre@staltz.com>
2018-09-26 13:31:15 +0300

    chore(META): fix bump script

--- .scripts/bump.js
+++ .scripts/bump.js
@@ -6,7 +6,9 @@ const packagePath = process.argv[2];
 const bumpType = process.argv[3];
 
 if (!packagePath || packagePath === '') {
-  console.log("Please provide a relative package.json path as the first argument");
+  console.log(
+    'Please provide a relative package.json path as the first argument'
+  );
   process.exit(1);
 }
 
@@ -14,19 +16,26 @@ const packageJSON = require(path.join(process.cwd(), packagePath));
 
 const version = packageJSON.version;
 
-let [major, minor, patch, ...others] = version.split('.').map(s => parseInt(s, 10));
+let [major, minor, patch, ...others] = version
+  .split('.')
+  .map(s => parseInt(s, 10));
 
 if (bumpType == '--major') {
   major += 1;
+  minor = 0;
+  patch = 0;
 } else if (bumpType == '--minor') {
   minor += 1;
+  patch = 0;
 } else if (bumpType == '--patch') {
   patch += 1;
 } else {
-  console.log("Please specify --major, --minor or --patch as the second argument");
+  console.log(
+    'Please specify --major, --minor or --patch as the second argument'
+  );
   process.exit(1);
 }
 
 packageJSON.version = [major, minor, patch, ...others].join('.');
 
-fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2) + "\n");
+fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2) + '\n');
