7f45d5be6f0b293adce48559c06c19c27f168787
Jan van Brügge <supermanitu@gmail.com>
2020-09-22 12:08:23 +0200

    Add source maps to the bundled code

--- configs/rollup.config.ts
+++ configs/rollup.config.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 const pkg = JSON.parse(readFileSync('./package.json', { encoding: 'utf-8' }));
 
@@ -7,8 +8,10 @@ const format = process.env.BUNDLE_FORMAT || 'esm';
 export default {
   input: 'build/index.js',
   external: pkg.dependencies ? Object.keys(pkg.dependencies) : [],
+  plugins: [sourcemaps()],
   output: {
     file: `build/bundle.${format}.js`,
+    sourcemap: true,
     format,
   },
 };
